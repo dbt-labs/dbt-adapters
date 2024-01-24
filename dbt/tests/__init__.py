da9fdd1d2c0438645ac7c776f2f542fc99f17a0a
Colin <colin.rogers@dbtlabs.com>
2024-01-24 11:05:21 -0800

    fix __init__.py

--- dbt/tests/__init__.py
+++ dbt/tests/__init__.py
@@ -1,6 +1,1 @@
-# N.B.
-# This will add to the package’s __path__ all subdirectories of directories on sys.path named after the package which effectively combines both modules into a single namespace (dbt.adapters)
 
-from pkgutil import extend_path
-
-__path__ = extend_path(__path__, __name__)
