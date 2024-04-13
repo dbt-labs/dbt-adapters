fface1d70c1443c855ccac241ec9101d0f5bd839
Doug Beatty <doug.beatty@dbtlabs.com>
2024-04-12 19:22:01 -0600

    Cross-database `cast` macro

---
+++ dbt-tests-adapter/dbt/tests/adapter/utils/test_cast.py
@@ -0,0 +1,25 @@
+import pytest
+
+from dbt.tests.adapter.utils import base_utils, fixture_cast
+
+
+class BaseSafeCast(base_utils.BaseUtils):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"data_cast.csv": fixture_cast.seeds__data_cast_csv}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_cast.yml": fixture_cast.models__test_cast_yml,
+            "test_cast.sql": self.interpolate_macro_namespace(
+                self.interpolate_macro_namespace(
+                    fixture_cast.models__test_cast_sql, "cast"
+                ),
+                "type_string",
+            ),
+        }
+
+
+class TestSafeCast(BaseSafeCast):
+    pass
