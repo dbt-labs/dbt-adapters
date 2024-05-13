3aba80e4e0c93d3395d681d2caf776bb24df9d41
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2024-05-13 17:33:31 -0600

    Cross-database `date` macro (#81)
    Co-authored-by: Colin Rogers <111200756+colin-rogers-dbt@users.noreply.github.com>

--- tests/functional/shared_tests/test_utils.py
+++ tests/functional/shared_tests/test_utils.py
@@ -8,6 +8,7 @@ from dbt.tests.adapter.utils.test_bool_or import BaseBoolOr
 from dbt.tests.adapter.utils.test_cast_bool_to_text import BaseCastBoolToText
 from dbt.tests.adapter.utils.test_concat import BaseConcat
 from dbt.tests.adapter.utils.test_current_timestamp import BaseCurrentTimestampAware
+from dbt.tests.adapter.utils.test_date import BaseDate
 from dbt.tests.adapter.utils.test_dateadd import BaseDateAdd
 from dbt.tests.adapter.utils.test_datediff import BaseDateDiff
 from dbt.tests.adapter.utils.test_date_spine import BaseDateSpine
@@ -66,6 +67,10 @@ class TestConcat(BaseConcat):
 
 
 class TestCurrentTimestamp(BaseCurrentTimestampAware):
+    pass
+
+
+class TestDate(BaseDate):
     pass
 
 
