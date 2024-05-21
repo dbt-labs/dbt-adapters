a9fef3b67409d1e7d1a581ff82b1021bd8e5e4d0
Anders Swanson <anders.swanson@dbtlabs.com>
2024-05-21 17:08:57 -0400

    ongoing work

--- dbt/include/global_project/macros/materializations/external_table/get_create_external_table.sql
+++ dbt/include/global_project/macros/materializations/external_table/get_create_external_table.sql
@@ -1,7 +1,7 @@
-{% macro create_external_table(source_node) %}
-    {{ adapter.dispatch('create_external_table', 'dbt')(source_node) }}
+{% macro create_external_table(relation, columns) %}
+    {{ adapter.dispatch('create_external_table', 'dbt')(relation, columns) }}
 {% endmacro %}
 
-{% macro default__create_external_table(source_node) %}
+{% macro default__create_external_table(relation, columns) %}
     {{ exceptions.raise_compiler_error("External table creation is not implemented for the default adapter") }}
 {% endmacro %}
