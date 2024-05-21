cf0b158d211be5c5c83b6d76fa7aa33731b3b927
Anders Swanson <anders.swanson@dbtlabs.com>
2024-05-21 15:24:44 -0400

    model materialization now

--- dbt/include/global_project/macros/materializations/external_table/external_table.sql
+++ dbt/include/global_project/macros/materializations/external_table/external_table.sql
@@ -1,13 +1,12 @@
-{% macro mat_external_table(source_node) %}
-    {{ adapter.dispatch('mat_external_table', 'dbt')(source_node) }}
-{% endmacro %}
+{% materialization external_table, default %}
 
-{% macro default__mat_external_table(source_node) %}
-
+  {%- set identifier = model['alias'] -%}
   {%- set full_refresh_mode = (should_full_refresh()) -%}
 
+  {# {%- set existing_relation = load_cached_relation(this) -%} #}
+  {%- set target_relation = this.incorporate(type='external_table') %}
 
-  {%- set old_relation = adapter.get_relation(database=source_node.database, schema=source_node.schema, identifier=source_node.name) -%}
+  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
 
   {%- set exists_as_table = (old_relation is not none and old_relation.is_table) -%}
   {%- set exists_as_view = (old_relation is not none and old_relation.is_view) -%}
@@ -43,6 +42,8 @@
   {% set code = 'CREATE' if create_or_replace else 'REFRESH' %}
 
   {{ log('XXX: build_plan: ' ~ build_plan, info=True) }}
+
+  no-op
   {% do run_query(build_plan) %}
 
 
@@ -61,11 +62,5 @@
   {{ run_hooks(post_hooks, inside_transaction=False) }}
 
   {{ return({'relations': [target_relation]}) }}
-{% endmacro %}
-
-{% materialization external_table, default %}
-
-  {{ mat_external_table(source_node) }}
-
 
 {% endmaterialization %}
