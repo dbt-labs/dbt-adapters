76b5f4d90268ed5b84edc8ac7f9d17234a5503ac
Anders Swanson <anders.swanson@dbtlabs.com>
2024-05-22 17:04:13 -0400

    improve control flow

--- dbt/include/global_project/macros/materializations/external_table/external_table.sql
+++ dbt/include/global_project/macros/materializations/external_table/external_table.sql
@@ -8,9 +8,6 @@
 
   {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
 
-  {{ log('old_relation.is_table: ' ~ old_relation.is_table, info=True) }}
-  {{ log('old_relation.is_external_table: ' ~ old_relation.is_external_table, info=True) }}
-
   {%- set exists_as_table = (old_relation is not none and old_relation.is_table) -%}
   {%- set exists_as_view = (old_relation is not none and old_relation.is_view) -%}
   {%- set exists_as_external_table = (old_relation is not none and old_relation.is_external_table) -%}
@@ -23,43 +20,30 @@
   {{ run_hooks(pre_hooks, inside_transaction=True) }}
 
   -- build model
-  {% set build_plan = "" %}
+  {% set build_plan = [] %}
 
-  {% set create_or_replace = (old_relation is none or full_refresh_mode) %}
-  {{ log('create_or_replace: ' ~ create_or_replace, info=True) }}
-
-{% if old_relation is not none %}
-  
-{% endif %}
-    
   {% if exists_as_view %}
     {{ exceptions.raise_compiler_error("Cannot make ExTab to '{}', it is already view".format(old_relation)) }}
    {% elif exists_as_table %}
-    {{ exceptions.raise_compiler_error("Cannot make ExTab '{}', it is a already a table".format(old_relation)) }}
-  {% elif exists_as_external_table %}
+    {# {{ exceptions.raise_compiler_error("Cannot make ExTab '{}', it is a already a table".format(old_relation)) }} #}
+  {# {% elif exists_as_external_table %} #}
     {% if full_refresh_mode %}
-      {% set build_plan = build_plan + create_external_table(target_relation, model.columns.values()) %}
+      {% set code = 'CREATE' %}
+      {% set build_plan = build_plan + [create_external_table(target_relation, model.columns.values())] %}
     {% elif not full_refresh_mode %} 
-      {% set build_plan = build_plan + refresh_external_table(source_node) %}
+      {% set code = 'REFRESH' %}
+      {% set build_plan = build_plan + refresh_external_table(target_relation) %}
     {% endif %}
   {% else %}
+    {% set code = 'CREATE' %}
     {% set build_plan = build_plan + [
-      create_external_schema(source_node),
-      create_external_table(source_node)
+      create_external_table(target_relation, model.columns.values())
     ] %}
   {% endif %}
-
-  {% set build_plan = create_external_table(target_relation, model.columns.values()) %}
-
-  {% set code = 'CREATE' if create_or_replace else 'REFRESH' %}
-
-  {{ log('XXX: build_plan: ' ~ build_plan, info=True) }}
 
   {% call noop_statement('main', code, code) %}
     {{ build_plan }};
   {% endcall %}
-
-  {% set target_relation = old_relation.incorporate(type='external_table') %}
 
   {% set should_revoke = should_revoke(old_relation, full_refresh_mode) %}
   {% do apply_grants(target_relation, grant_config, should_revoke=should_revoke) %}
