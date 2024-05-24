2d5d9d6b232037ad0e4bd4d99017757d15a36ff2
Anders Swanson <anders.swanson@dbtlabs.com>
2024-05-24 15:05:41 -0400

    force rebuild if exists as view

--- dbt/include/global_project/macros/materializations/external_table/external_table.sql
+++ dbt/include/global_project/macros/materializations/external_table/external_table.sql
@@ -3,39 +3,33 @@
   {%- set identifier = model['alias'] -%}
   {%- set full_refresh_mode = (should_full_refresh()) -%}
 
-  {# {%- set existing_relation = load_cached_relation(this) -%} #}
-  {%- set target_relation = this.incorporate(type='external_table') %}
+  {%- set existing_relation = load_cached_relation(this) -%}
+  {%- set target_relation = this.incorporate(type=this.ExternalTable) %}
 
-  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
+  {{ log('existing_relation: ' ~ existing_relation, info=True) }}
+  {{ log('existing_relation.type: ' ~ existing_relation.type, info=True) }}
 
-  {%- set exists_as_table = (old_relation is not none and old_relation.is_table) -%}
-  {%- set exists_as_view = (old_relation is not none and old_relation.is_view) -%}
-  {%- set exists_as_external_table = (old_relation is not none and old_relation.is_external_table) -%}
-
-  {%- set grant_config = config.get('grants') -%}
-
-  {{ run_hooks(pre_hooks, inside_transaction=False) }}
-
-  -- `BEGIN` happens here:
-  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+  {%- set exists_as_table = (existing_relation is not none and existing_relation.is_table) -%}
+  {%- set exists_as_view = (existing_relation is not none and existing_relation.is_view) -%}
+  {%- set exists_as_external_table = (existing_relation is not none and existing_relation.is_external_table) -%}
 
   -- build model
   {% set build_plan = [] %}
+  {% set code = 'CREATE' %}
 
   {% if exists_as_view %}
-    {{ exceptions.raise_compiler_error("Cannot make ExTab to '{}', it is already view".format(old_relation)) }}
+      {% set build_plan = build_plan + [
+      drop_relation_if_exists(existing_relation),
+      create_external_table(target_relation, model.columns.values())
+    ] %}
    {% elif exists_as_table %}
-    {# {{ exceptions.raise_compiler_error("Cannot make ExTab '{}', it is a already a table".format(old_relation)) }} #}
-  {# {% elif exists_as_external_table %} #}
     {% if full_refresh_mode %}
-      {% set code = 'CREATE' %}
       {% set build_plan = build_plan + [create_external_table(target_relation, model.columns.values())] %}
     {% elif not full_refresh_mode %} 
       {% set code = 'REFRESH' %}
       {% set build_plan = build_plan + refresh_external_table(target_relation) %}
     {% endif %}
   {% else %}
-    {% set code = 'CREATE' %}
     {% set build_plan = build_plan + [
       create_external_table(target_relation, model.columns.values())
     ] %}
@@ -45,7 +39,9 @@
     {{ build_plan }};
   {% endcall %}
 
-  {% set should_revoke = should_revoke(old_relation, full_refresh_mode) %}
+  {%- set grant_config = config.get('grants') -%}
+
+  {% set should_revoke = should_revoke(existing_relation, full_refresh_mode) %}
   {% do apply_grants(target_relation, grant_config, should_revoke=should_revoke) %}
 
   {% do persist_docs(target_relation, model) %}
