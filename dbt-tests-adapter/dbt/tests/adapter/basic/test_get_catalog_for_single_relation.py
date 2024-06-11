dfca5f14513840d36c9f026281ed4bfeae15ff14
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2024-06-11 17:31:11 +0100

    add test for catalog

--- dbt-tests-adapter/dbt/tests/adapter/basic/test_get_catalog_for_single_relation.py
+++ dbt-tests-adapter/dbt/tests/adapter/basic/test_get_catalog_for_single_relation.py
@@ -1,7 +1,13 @@
 import pytest
 
 from dbt.adapters.capability import Capability
-
+from dbt_common.contracts.metadata import (
+    TableMetadata,
+    StatsItem,
+    CatalogTable,
+    ColumnMetadata,
+)
+from dbt.tests.util import run_dbt, get_connection
 
 models__my_model_sql = """
 {{
@@ -10,7 +16,7 @@ models__my_model_sql = """
     )
 }}
 
-select * from {{ ref('seed') }}
+select * from {{ ref('my_seed') }}
 """
 
 seed__my_seed_csv = """id,first_name,email,ip_address,updated_at
@@ -19,6 +25,10 @@ seed__my_seed_csv = """id,first_name,email,ip_address,updated_at
 
 
 class BaseGetCatalogForSingleRelation:
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"name": "get_catalog_for_single_relation"}
+
     @pytest.fixture(scope="class")
     def seeds(self):
         return {
@@ -40,22 +50,73 @@ class BaseGetCatalogForSingleRelation:
             )
             project.adapter.drop_schema(relation)
 
+    pass
+
     def test_get_catalog_for_single_relation(self, project):
-        adapter = project.adapter
+        results = run_dbt(["seed"])
+        assert len(results) == 1
 
-        if adapter.supports(Capability.GetCatalogForSingleRelation):
-            my_model_relation = adapter.Relation.create(
-                database=project.database,
-                schema=project.test_schema,
-                identifier="my_model",
+        results = run_dbt(["run"])
+        assert len(results) == 1
+
+        if project.adapter.supports(Capability.GetCatalogForSingleRelation):
+            expected = CatalogTable(
+                metadata=TableMetadata(
+                    type="VIEW",
+                    schema=project.test_schema.upper(),
+                    name="MY_MODEL",
+                    database=project.database,
+                    comment="",
+                    owner="TESTER",
+                ),
+                columns={
+                    "ID": ColumnMetadata(type="NUMBER", index=1, name="ID", comment=None),
+                    "FIRST_NAME": ColumnMetadata(
+                        type="VARCHAR", index=2, name="FIRST_NAME", comment=None
+                    ),
+                    "EMAIL": ColumnMetadata(type="VARCHAR", index=3, name="EMAIL", comment=None),
+                    "IP_ADDRESS": ColumnMetadata(
+                        type="VARCHAR", index=4, name="IP_ADDRESS", comment=None
+                    ),
+                    "UPDATED_AT": ColumnMetadata(
+                        type="TIMESTAMP_NTZ", index=5, name="UPDATED_AT", comment=None
+                    ),
+                },
+                stats={
+                    "has_stats": StatsItem(
+                        id="has_stats",
+                        label="Has Stats?",
+                        value=True,
+                        include=False,
+                        description="Indicates whether there are statistics for this table",
+                    ),
+                    "row_count": StatsItem(
+                        id="row_count",
+                        label="Row Count",
+                        value=0,
+                        include=True,
+                        description="Number of rows in the table as reported by Snowflake",
+                    ),
+                    "bytes": StatsItem(
+                        id="bytes",
+                        label="Approximate Size",
+                        value=0,
+                        include=True,
+                        description="Size of the table as reported by Snowflake",
+                    ),
+                },
+                unique_id=None,
             )
 
-            expected_catalog_table = 1
+            with get_connection(project.adapter):
+                my_model_relation = project.adapter.get_relation(
+                    database=project.database,
+                    schema=project.test_schema,
+                    identifier="MY_MODEL",
+                )
+                actual = project.adapter.get_catalog_for_single_relation(my_model_relation)
 
-            assert (
-                project.adapter.get_catalog_for_single_relation(my_model_relation)
-                == expected_catalog_table
-            )
+                assert actual == expected
 
 
 class TestGetCatalogForSingleRelation(BaseGetCatalogForSingleRelation):
