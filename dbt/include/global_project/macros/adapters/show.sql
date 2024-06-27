f6c455f1e3a1710f71a40fbca4dc084048e268e1
Mila Page <versusfacit@users.noreply.github.com>
2024-06-27 15:40:29 -0700

    adjust whitespace for bigquery.

--- dbt/include/global_project/macros/adapters/show.sql
+++ dbt/include/global_project/macros/adapters/show.sql
@@ -6,7 +6,7 @@
 {% macro get_show_sql(compiled_code, sql_header, limit) -%}
   {%- if sql_header is not none -%}
   {{ sql_header }}
-  {%- endif -%}
+  {%- endif %}
   {{ get_limit_subquery_sql(compiled_code, limit) }}
 {% endmacro %}
 
@@ -14,13 +14,13 @@
     Not necessarily a true subquery anymore. Now, merely a query subordinate
     to the calling macro.
 #}
-{% macro get_limit_subquery_sql(sql, limit) %}
+{%- macro get_limit_subquery_sql(sql, limit) -%}
   {{ adapter.dispatch('get_limit_sql', 'dbt')(sql, limit) }}
-{% endmacro %}
+{%- endmacro -%}
 
 {% macro default__get_limit_sql(sql, limit) %}
   {{ compiled_code }}
-  {%- if limit is not none %}
+  {% if limit is not none %}
   limit {{ limit }}
   {%- endif -%}
 {% endmacro %}
