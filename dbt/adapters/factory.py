e3021be357b35b3eb6b2ecba91e867e58df3ae31
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2024-06-28 14:52:25 -0600

    Update other type hint for `get_adapter_constraint_support`

--- dbt/adapters/factory.py
+++ dbt/adapters/factory.py
@@ -251,7 +251,7 @@ def get_adapter_type_names(name: Optional[str]) -> List[str]:
     return FACTORY.get_adapter_type_names(name)
 
 
-def get_adapter_constraint_support(name: Optional[str]) -> List[str]:
+def get_adapter_constraint_support(name: Optional[str]) -> Dict[str, str]:
     return FACTORY.get_adapter_constraint_support(name)
 
 
