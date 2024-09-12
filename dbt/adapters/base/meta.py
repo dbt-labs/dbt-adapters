790dc9e02aca734cb66a2fe179170c12f659a5ed
Mike Alfare <mike.alfare@dbtlabs.com>
2024-09-12 10:49:41 -0400

    make behavior available on BaseAdapter in the jinja context

--- dbt/adapters/base/meta.py
+++ dbt/adapters/base/meta.py
@@ -15,6 +15,10 @@ class _Available:
         func._is_available_ = True  # type: ignore
         return func
 
+    def __get__(self, obj, objtype=None) -> Any:
+        obj._is_available_ = True  # type: ignore
+        return obj
+
     def parse(self, parse_replacement: Callable) -> Decorator:
         """A decorator factory to indicate that a method on the adapter will be
         exposed to the database wrapper, and will be stubbed out at parse time
