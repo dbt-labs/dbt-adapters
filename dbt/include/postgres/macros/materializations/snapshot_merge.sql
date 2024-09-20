d69a12f27a62a6ccc0366b2f9e7155938337b384
Gerda Shank <gerda@dbtlabs.com>
2024-09-20 16:33:06 -0400

    Allow configuring snapshot table column names (#145)

--- dbt/include/postgres/macros/materializations/snapshot_merge.sql
+++ dbt/include/postgres/macros/materializations/snapshot_merge.sql
@@ -2,12 +2,14 @@
 {% macro postgres__snapshot_merge_sql(target, source, insert_cols) -%}
     {%- set insert_cols_csv = insert_cols | join(', ') -%}
 
+    {%- set columns = config.get("snapshot_table_column_names") or get_snapshot_table_column_names() -%}
+
     update {{ target }}
-    set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
+    set {{ columns.dbt_valid_to }} = DBT_INTERNAL_SOURCE.{{ columns.dbt_valid_to }}
     from {{ source }} as DBT_INTERNAL_SOURCE
-    where DBT_INTERNAL_SOURCE.dbt_scd_id::text = {{ target }}.dbt_scd_id::text
+    where DBT_INTERNAL_SOURCE.{{ columns.dbt_scd_id }}::text = {{ target }}.{{ columns.dbt_scd_id }}::text
       and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
-      and {{ target }}.dbt_valid_to is null;
+      and {{ target }}.{{ columns.dbt_valid_to }} is null;
 
     insert into {{ target }} ({{ insert_cols_csv }})
     select {% for column in insert_cols -%}
