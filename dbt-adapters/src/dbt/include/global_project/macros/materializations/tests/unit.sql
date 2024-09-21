2cdd68a3ceb4102c1e82f9febd4b71506ab69784
Takeshi Mikami <takeshi.mikami@gmail.com>
2024-09-21 20:24:26 +0900

    patch for bigquery unittest with sql_header

--- dbt-adapters/src/dbt/include/global_project/macros/materializations/tests/unit.sql
+++ dbt-adapters/src/dbt/include/global_project/macros/materializations/tests/unit.sql
@@ -1,3 +1,12 @@
+{% macro get_create_table_as_sql_with_sql_header(temporary, relation, sql, sql_header) -%}
+  {{ sql_header }}
+  {{ adapter.dispatch('get_create_table_as_sql', 'dbt')(temporary, relation, sql) }}
+{%- endmacro %}
+{% macro get_unit_test_sql_with_sql_header(main_sql, expected_fixture_sql, expected_column_names, sql_header) -%}
+  {{ sql_header }}
+  {{ adapter.dispatch('get_unit_test_sql', 'dbt')(main_sql, expected_fixture_sql, expected_column_names) }}
+{%- endmacro %}
+
 {%- materialization unit, default -%}
 
   {% set relations = [] %}
@@ -8,7 +17,8 @@
 
   {%- set target_relation = this.incorporate(type='table') -%}
   {%- set temp_relation = make_temp_relation(target_relation)-%}
-  {% do run_query(get_create_table_as_sql(True, temp_relation, get_empty_subquery_sql(sql))) %}
+  {%- set sql_header = tested_node_config.get("sql_header", "") if tested_node_config is defined else "" -%}
+  {% do run_query(get_create_table_as_sql_with_sql_header(True, temp_relation, get_empty_subquery_sql(sql), sql_header)) %}
   {%- set columns_in_relation = adapter.get_columns_in_relation(temp_relation) -%}
   {%- set column_name_to_data_types = {} -%}
   {%- for column in columns_in_relation -%}
@@ -18,7 +28,7 @@
   {% if not expected_sql %}
   {%   set expected_sql = get_expected_sql(expected_rows, column_name_to_data_types) %}
   {% endif %}
-  {% set unit_test_sql = get_unit_test_sql(sql, expected_sql, tested_expected_column_names) %}
+  {% set unit_test_sql = get_unit_test_sql_with_sql_header(sql, expected_sql, tested_expected_column_names, sql_header) %}
 
   {% call statement('main', fetch_result=True) -%}
 
