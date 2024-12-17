70afd18a8e5f63652935914ef325a46471fcc8db
Adrian Burus <adrian.burus@dbtlabs.com>
2024-12-17 11:28:00 +0200

    Update equals.sql macro

--- dbt/include/global_project/macros/utils/equals.sql
+++ dbt/include/global_project/macros/utils/equals.sql
@@ -1,11 +1,12 @@
-{% macro equals(first_date, second_date, datepart) -%}
-    {{ return(adapter.dispatch('equals', 'dbt') (expression)) }}
+{% macro equals(first_date, second_date, datepart) %}
+    {{ return(adapter.dispatch('equals', 'dbt') (expr1, expr2)) }}
 {%- endmacro %}
 
-{% macro defaults__equals(expr1, expr2) -%}
-case when (({{ expr1 }} = {{ expr2 }}) or ({{ expr1 }} is null and {{ expr2 }} is null))
-    then 0
-    else 1
-end = 0
-{% endmacro %}
+{% macro default__equals(expr1, expr2) -%}
 
+    case when (({{ expr1 }} = {{ expr2 }}) or ({{ expr1 }} is null and {{ expr2 }} is null))
+        then 0
+        else 1
+    end = 0
+
+{% endmacro %}
