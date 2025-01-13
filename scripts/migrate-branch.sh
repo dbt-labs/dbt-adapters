6427ec32b1b6940379df1f624e087ab18085ddc1
Mike Alfare <mike.alfare@dbtlabs.com>
2025-01-13 15:39:23 -0500

    create a script to migrate feature branches from legacy adapter repos and forks

--- scripts/migrate-branch.sh
+++ scripts/migrate-branch.sh
@@ -1,18 +1,23 @@
-_USER=$1  # your github user
-_BRANCH=$2  # your feature branch
-_REPO=$3  # the repo name (the adapter)
+# 1. Run this script to migrate your feature branch from an adapter repo/fork to the dbt-adapters repo/fork, e.g.:
+#    $> source ./scripts/migrate-branch.sh dbt-labs dbt-postgres my-cool-feature-branch
+# 2. Resolve any resulting merge conflicts.
+# 3. Push the resulting branch back up to the dbt-adapters repo/fork, e.g.:
+#    $> git push origin dbt-postgres/my-cool-feature-branch
 
-# create a remote for the adapter repo (supports forks via _USER)
-git remote add adapter https://github.com/$_USER/$_REPO.git
+user=$1    # your github user (e.g. dbt-labs)
+repo=$2    # the repo name (e.g. dbt-postgres)
+branch=$3  # your feature branch (e.g. my-cool-feature-branch)
+
+# create a remote for the adapter repo (supports forks)
+git remote remove adapter || true
+git remote add adapter https://github.com/$user/$repo.git
 git fetch adapter
 
-# update your feature branch against dbt-adapters@main
-git rebase main adapter/$_BRANCH
-
 # create a branch in the dbt-adapters repo for your feature branch from the adapter repo
-git checkout -b $_REPO/$_BRANCH  # prefixing <adapter>/ namespaces your feature branch in the new repo
-git merge adapter/$_BRANCH
-git push origin $_REPO/$_BRANCH
+git checkout -b $repo/$branch  # prefixing <adapter>/ namespaces your feature branch in the new repo
+
+# update your feature branch against dbt-adapters@main
+git merge adapter/$branch
 
 # remove the remote that was created by this process
 git remote remove adapter
