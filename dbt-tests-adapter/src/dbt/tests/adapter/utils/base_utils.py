6abab94fad3d3a8b8682bf3b9e4d271457f48795
VersusFacit <67295367+VersusFacit@users.noreply.github.com>
2025-01-27 22:56:38 -0800

    restore macro definition headers.

--- dbt-tests-adapter/src/dbt/tests/adapter/utils/base_utils.py
+++ dbt-tests-adapter/src/dbt/tests/adapter/utils/base_utils.py
@@ -3,6 +3,7 @@ from dbt.tests.util import run_dbt
 
 
 macros__equals_sql = """
+{% macro equals(expr1, expr2) -%}
 {%- if adapter.behavior.enable_truthy_nulls_equals_macro.no_warn %}
     case when (({{ expr1 }} = {{ expr2 }}) or ({{ expr1 }} is null and {{ expr2 }} is null))
 {%- else -%}
@@ -11,6 +12,7 @@ macros__equals_sql = """
         then 0
         else 1
     end = 0
+{% endmacro %}
 """
 
 macros__test_assert_equal_sql = """
