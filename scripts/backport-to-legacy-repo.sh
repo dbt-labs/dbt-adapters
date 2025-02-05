6efba2ea74544a7682adef9026da56671fc77605
Mike Alfare <mike.alfare@dbtlabs.com>
2025-02-05 15:45:17 -0500

    update the backport script to run in the monorepo

--- scripts/backport-to-legacy-repo.sh
+++ scripts/backport-to-legacy-repo.sh
@@ -1,17 +1,30 @@
 # this script allows you to backport a change from dbt-adapters into a legacy adapter repo
-# it should be run from the adapter repo, but because it's the same script for all adapters, it's saved here
-# TODO: update this script to run from here
 
-$COMMIT
+ADAPTER=$1
+BRANCH=$2
+COMMIT=$3
 
-git remote add monorepo https://github.com/dbt-labs/dbt-adapters.git
+# add a new remote for the legacy repo
+git remote add $ADAPTER https://github.com/dbt-labs/$ADAPTER.git
 git fetch
 
+# create a new branch off of the target branch
+git switch $ADAPTER/$BRANCH
+git switch -c $ADAPTER/backport-$COMMIT-to-$BRANCH
+
+# cherry pick the commit from dbt-adapters into the new branch
 git cherry-pick $COMMIT -x
 
 # manually move new files where they need to be
 # in particular, new files will not be moved automatically since there is no reference in the target repo
 
+# continue the cherry pick process after resolving conflicts
 git cherry-pick --continue -e
 
-git remote remove monorepo
+# :x!<enter> to accept the message
+
+# push the new branch up to the legacy repo
+git push $ADAPTER backport-$COMMIT-to-$BRANCH
+
+# remove the remote that was created during this process
+git remote remove $ADAPTER
