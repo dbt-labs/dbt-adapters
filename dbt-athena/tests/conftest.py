68bab5a3560b3b920da45c0f4adc00c50e03611b
Tudor Leustean <tudor.leustean@dbtlabs.com>
2025-02-07 15:50:26 +0200

    linting

--- dbt-athena/tests/conftest.py
+++ dbt-athena/tests/conftest.py
@@ -1,13 +1,3 @@
-# in order to call dbt's internal profile rendering, we need to set the
-# flags global. This is a bit of a hack, but it's the best way to do it.
-from dbt.flags import set_from_args
-from argparse import Namespace
-
-set_from_args(Namespace(), None)
-
-pytest_plugins = "dbt.tests.fixtures.project"
-
-
 def pytest_sessionfinish(session, exitstatus):
     """
     Configures pytest to treat a scenario with no tests as passing
