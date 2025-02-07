9867261e352ab0341d54c4e539425a6aa588be6c
Colin <colin.rogers@dbtlabs.com>
2025-02-07 15:53:19 -0800

    Add Functional Tests

--- dbt-snowflake/tests/functional/adapter/catalog_integrations/test_managed_iceberg_catalog.py
+++ dbt-snowflake/tests/functional/adapter/catalog_integrations/test_managed_iceberg_catalog.py
@@ -2,18 +2,44 @@ import pytest
 from dbt.tests.util import run_dbt, write_config_file
 from dbt.tests.adapter.catalog_integrations.test_catalog_integration import BaseCatalogIntegration
 
+BASIC_ICEBERG_TABLE_MODEL = """
+{{
+  config(
+    materialized = "table",
+    catalog_name = "test_catalog",
+  )
+}}
+select * from 1 as id
+"""
 
 class TestManagedIcebergCatalogIntegration(BaseCatalogIntegration):
 
-    @pytest.fixture(scope="class", autouse=True)
-    def write_catalog_integration(self, project):
+    @pytest.fixture
+    def catalogs(self, project):
         return {
-            "name": "write_integration_name",
+            "catalogs": [
+                {
+                    "name": "test_catalog",
+                    "write_integrations": [
+                        {
+                            "name": "write_integration_name",
             "external_volume": "s3_iceberg_snow",
             "table_format": "iceberg",
             "catalog_type": "managed",
+        }
+                    ],
+                }
+            ]
         }
 
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {"flags": {"enable_iceberg_materializations": True}}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "basic_table.sql": BASIC_ICEBERG_TABLE_MODEL,
+        }
+    def test_managed_iceberg_catalog_integration(self, project):
+        run_dbt(["run"])
