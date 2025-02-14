6525e31d4814df3d20bab50fa5de578013c9ca20
Colin Rogers <111200756+colin-rogers-dbt@users.noreply.github.com>
2025-02-14 13:45:40 -0800

    handle null result from backend_pid check (#821)

--- dbt-redshift/src/dbt/adapters/redshift/connections.py
+++ dbt-redshift/src/dbt/adapters/redshift/connections.py
@@ -416,7 +416,9 @@ class RedshiftConnectionManager(SQLConnectionManager):
         with connection.handle.cursor() as c:
             sql = "select pg_backend_pid()"
             res = c.execute(sql).fetchone()
-        return res[0]
+        if res:
+            return res[0]
+        return None
 
     @classmethod
     def get_response(cls, cursor: redshift_connector.Cursor) -> AdapterResponse:
@@ -492,7 +494,9 @@ class RedshiftConnectionManager(SQLConnectionManager):
             retry_limit=credentials.retries,
             retryable_exceptions=retryable_exceptions,
         )
-        open_connection.backend_pid = cls._get_backend_pid(open_connection)
+
+        if backend_pid := cls._get_backend_pid(open_connection):
+            open_connection.backend_pid = backend_pid
         return open_connection
 
     def execute(
