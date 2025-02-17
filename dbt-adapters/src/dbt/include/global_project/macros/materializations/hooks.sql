a82ba2bdfc11e8ea70c6c4557551f7a1bba83ccf
Vengal Rao Guttha <vengalrao.guttha@snowflake.com>
2025-02-17 11:57:24 +0530

    refactor

--- dbt-adapters/src/dbt/include/global_project/macros/materializations/hooks.sql
+++ dbt-adapters/src/dbt/include/global_project/macros/materializations/hooks.sql
@@ -1,23 +1,25 @@
 {% macro run_hooks(hooks, inside_transaction=True) %}
-  {% set tracer = modules.opentelemetry.trace.get_tracer("dbt-runner") %}
-  {% set hook_span = tracer.start_span('hooks', context=modules.opentelemetry.context.get_current())%}
-  {% set ctx = modules.opentelemetry.trace.set_span_in_context(hook_span) %}
-  {% set token = modules.opentelemetry.context.attach(ctx) %}
-  {% for hook in hooks | selectattr('transaction', 'equalto', inside_transaction)  %}
-    {% if not inside_transaction and loop.first %}
-      {% call statement(auto_begin=inside_transaction) %}
-        commit;
-      {% endcall %}
-    {% endif %}
-    {% set rendered = render(hook.get('sql')) | trim %}
-    {% if (rendered | length) > 0 %}
-      {% call statement(auto_begin=inside_transaction) %}
-        {{ rendered }}
-      {% endcall %}
-    {% endif %}
-  {% endfor %}
-  {% set _ = modules.opentelemetry.context.detach(token) %}
-  {% set _ = hook_span.end() %}
+  {% if len(hooks) > 0 %}
+      {% set tracer = modules.opentelemetry.trace.get_tracer("dbt-runner") %}
+      {% set hook_span = tracer.start_span('hooks', context=modules.opentelemetry.context.get_current())%}
+      {% set ctx = modules.opentelemetry.trace.set_span_in_context(hook_span) %}
+      {% set token = modules.opentelemetry.context.attach(ctx) %}
+      {% for hook in hooks | selectattr('transaction', 'equalto', inside_transaction)  %}
+        {% if not inside_transaction and loop.first %}
+          {% call statement(auto_begin=inside_transaction) %}
+            commit;
+          {% endcall %}
+        {% endif %}
+        {% set rendered = render(hook.get('sql')) | trim %}
+        {% if (rendered | length) > 0 %}
+          {% call statement(auto_begin=inside_transaction) %}
+            {{ rendered }}
+          {% endcall %}
+        {% endif %}
+      {% endfor %}
+      {% set _ = modules.opentelemetry.context.detach(token) %}
+      {% set _ = hook_span.end() %}
+  {% endif %}
 {% endmacro %}
 
 
