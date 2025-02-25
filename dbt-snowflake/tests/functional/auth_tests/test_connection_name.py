94c11af358b4a8815b115ac785c8cd9845e32397
Dan Flippo <dan.flippo@snowflake.com>
2025-02-25 14:01:00 -0500

    Moved initialization logic to new fixture

--- dbt-snowflake/tests/functional/auth_tests/test_connection_name.py
+++ dbt-snowflake/tests/functional/auth_tests/test_connection_name.py
@@ -19,6 +19,16 @@ profiles.yml, we can test that we can connect based on credentials in the connec
 
 """
 
+connections_toml_template = f"""[default]
+account = "{account}"
+authenticator = "snowflake"
+database = "{database}"
+password = "{password}"
+role = "{role}"
+user = "{user}"
+warehouse = "{warehouse}"
+"""
+
 import os
 import pytest
 import tempfile
@@ -27,25 +37,17 @@ from dbt.tests.util import run_dbt
 
 
 class TestConnectionName:
-    @pytest.fixture(scope="class", autouse=True)
-    def dbt_profile_target(self):
 
+    @pytest.fixture(scope="class", autouse=True)
+    def connection_name(self):
         # We are creating a toml file that contains the password
-        home_dir = os.getenv("HOME")
-        snowflake_home_dir = os.getenv("SNOWFLAKE_HOME")
-        # Use the snowflake home if available
-        if snowflake_home_dir != None:
-            config_toml = os.path.join(snowflake_home_dir, "config.toml")
-            connections_toml = os.path.join(snowflake_home_dir, "connections.toml")
-        else:
-            config_toml = os.path.join(home_dir, ".snowflake", "config.toml")
-            connections_toml = os.path.join(home_dir, ".snowflake", "connections.toml")
-            snowflake_home_dir = os.path.join(home_dir, ".snowflake")
-            os.environ["SNOWFLAKE_HOME"] = snowflake_home_dir
+        snowflake_home_dir = os.path.expanduser("~/.snowflake")
+        config_toml = os.path.join(snowflake_home_dir, "config.toml")
+        connections_toml = os.path.join(snowflake_home_dir, "connections.toml")
+        os.environ["SNOWFLAKE_HOME"] = snowflake_home_dir
 
         if not os.path.exists(snowflake_home_dir):
             os.makedirs(snowflake_home_dir)
-        os.chmod(snowflake_home_dir, 0o700)
 
         with open(config_toml, "w") as f:
             f.write('default_connection_name = "default"')
@@ -53,19 +55,25 @@ class TestConnectionName:
 
         with open(connections_toml, "w") as f:
             f.write(
-                f"""
-[default]
-account = "{ os.getenv("SNOWFLAKE_TEST_ACCOUNT") }"
-authenticator = "snowflake"
-database = "{ os.getenv("SNOWFLAKE_TEST_DATABASE") }"
-password = "{ os.getenv("SNOWFLAKE_TEST_PASSWORD") }"
-role = "{ os.getenv("DBT_TEST_USER_1") }"
-user = "{ os.getenv("SNOWFLAKE_TEST_USER") }"
-warehouse = "{ os.getenv("SNOWFLAKE_TEST_WAREHOUSE") }"
-"""
+                connections_toml_template.format(
+                    account=os.getenv("SNOWFLAKE_TEST_ACCOUNT"),
+                    database=os.getenv("SNOWFLAKE_TEST_DATABASE"),
+                    password=os.getenv("SNOWFLAKE_TEST_PASSWORD"),
+                    role=os.getenv("SNOWFLAKE_TEST_ROLE"),
+                    user=os.getenv("SNOWFLAKE_TEST_USER"),
+                    warehouse=os.getenv("SNOWFLAKE_TEST_WAREHOUSE"),
+                ).strip()
             )
         os.chmod(connections_toml, 0o600)
 
+        yield "default"
+
+        del os.environ["SNOWFLAKE_HOME"]
+        os.unlink(config_toml)
+        os.unlink(connections_toml)
+
+    @pytest.fixture(scope="class", autouse=True)
+    def dbt_profile_target(self, connection_name):
         # We are returning a profile that does not contain the password
         return {
             "type": "snowflake",
@@ -73,7 +81,7 @@ warehouse = "{ os.getenv("SNOWFLAKE_TEST_WAREHOUSE") }"
             "account": os.getenv("SNOWFLAKE_TEST_ACCOUNT"),
             "database": os.getenv("SNOWFLAKE_TEST_DATABASE"),
             "warehouse": os.getenv("SNOWFLAKE_TEST_WAREHOUSE"),
-            "connection_name": "default",
+            "connection_name": connection_name,
         }
 
     @pytest.fixture(scope="class")
