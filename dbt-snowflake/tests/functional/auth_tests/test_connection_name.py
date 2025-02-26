c3ef04b2025ac2858de7ec53a2355eacfdcd9c71
Dan Flippo <dan.flippo@snowflake.com>
2025-02-26 17:35:08 -0500

    Switched to having the profile use ~ and setting HOME to tmp_path in test

--- dbt-snowflake/tests/functional/auth_tests/test_connection_name.py
+++ dbt-snowflake/tests/functional/auth_tests/test_connection_name.py
@@ -1,6 +1,7 @@
 """
-This class creates a connections.toml file at ~/.snowflake/connections.toml
-or a different folder using env variable SNOWFLAKE_HOME.
+This class sets the profile to use a connection_name and connections_file_path.
+The connection_name is "default" and the connections.toml file is set to "~/connections.toml"
+During the test we set the HOME to a temporary folder where we write this file.
 
 The script will populate the toml file based on the testing environment variables
 but will look something like:
@@ -14,8 +15,9 @@ role = "SNOWFLAKE_TEST_ROLE"
 user = "SNOWFLAKE_TEST_USER"
 warehouse = "SNOWFLAKE_TEST_WAREHOUSE"
 
-By putting the password in the connections.toml file and the connection_name in the
-profiles.yml, we can test that we can connect based on credentials in the connections.toml
+By putting the password in the connections.toml file and the connection_name
+& connections_file_path in the profiles.yml, we can test that we can connect
+based on credentials in the connections.toml
 
 """
 
@@ -23,6 +25,7 @@ from dbt.tests.util import run_dbt
 import tempfile
 import pytest
 import os
+from pathlib import Path
 
 connections_toml_template = """
 [{name}]
@@ -39,7 +42,7 @@ warehouse = "{warehouse}"
 class TestConnectionName:
 
     @pytest.fixture(scope="class", autouse=True)
-    def dbt_profile_target(self, tmp_path):
+    def dbt_profile_target(self):
         # We are returning a profile that does not contain the password
         return {
             "type": "snowflake",
@@ -48,7 +51,7 @@ class TestConnectionName:
             "database": os.getenv("SNOWFLAKE_TEST_DATABASE"),
             "warehouse": os.getenv("SNOWFLAKE_TEST_WAREHOUSE"),
             "connection_name": "default",
-            "connections_file_path": tmp_path / "connections.toml",
+            "connections_file_path": "~/connections.toml",
         }
 
     @pytest.fixture(scope="class")
@@ -56,23 +59,26 @@ class TestConnectionName:
         return {"my_model.sql": "select 1 as id"}
 
     # Test that we can write a connections.toml and use it to connect
-    def test_connection(self, project, dbt_profile_target):
-        connections_toml = dbt_profile_target.connections_file_path
+    def test_connection(self, project, tmp_path, monkeypatch):
 
-        # We are creating a toml file that contains the password
-        connections_toml.write_text(
-            connections_toml_template.format(
-                name=dbt_profile_target.connection_name,
-                account=dbt_profile_target.account,
-                database=dbt_profile_target.database,
-                password=os.getenv("SNOWFLAKE_TEST_PASSWORD"),
-                role=os.getenv("SNOWFLAKE_TEST_ROLE"),
-                user=os.getenv("SNOWFLAKE_TEST_USER"),
-                warehouse=dbt_profile_target.warehouse,
+        with monkeypatch.context() as m:
+            # Set HOME to our temporary folder for later tilde expansion in the driver
+            m.setenv("HOME", str(tmp_path.absolute()))
+
+            connections_toml = tmp_path / "connections.toml"
+
+            # We are creating a toml file that contains the password
+            connections_toml.write_text(
+                connections_toml_template.format(
+                    name="default",
+                    account=os.getenv("SNOWFLAKE_TEST_ACCOUNT"),
+                    database=os.getenv("SNOWFLAKE_TEST_DATABASE"),
+                    password=os.getenv("SNOWFLAKE_TEST_PASSWORD"),
+                    role=os.getenv("SNOWFLAKE_TEST_ROLE"),
+                    user=os.getenv("SNOWFLAKE_TEST_USER"),
+                    warehouse=os.getenv("SNOWFLAKE_TEST_WAREHOUSE"),
+                )
             )
-        )
-        connections_toml.chmod(0o600)
+            connections_toml.chmod(0o600)
 
-        run_dbt()
-
-        connections_toml.unlink()
+            run_dbt()
