84f02abd982b39c91755031bb4acf259fe4e6842
VersusFacit <67295367+VersusFacit@users.noreply.github.com>
2025-02-28 07:38:11 -0800

    port over PR.
    I researched this and things do look solid. I made sure as well this won't break anyone. I can't think of a case where this change of behavior will since I don't know who will be using the render limited _dbt specific aliases

--- dbt-spark/tests/functional/adapter/empty/test_empty.py
+++ dbt-spark/tests/functional/adapter/empty/test_empty.py
@@ -3,3 +3,7 @@ from dbt.tests.adapter.empty.test_empty import BaseTestEmpty
 
 class TestSparkEmpty(BaseTestEmpty):
     pass
+
+
+class TestSparkEmptyInlineSourceRef(BaseTestEmptyInlineSourceRef):
+    pass
