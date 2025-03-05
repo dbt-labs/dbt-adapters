b2a90d91b85c5e4aef3ccb8cb94652b28b667a87
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-05 18:38:49 -0500

    issue a warning when running dbt-athena-community and point users to dbt-athena

--- dbt-athena-community/src/dbt/adapters/athena_community/__init__.py
+++ dbt-athena-community/src/dbt/adapters/athena_community/__init__.py
@@ -1,1 +1,7 @@
 # this is a shell package that allows us to publish dbt-athena as dbt-athena-community
+import warnings
+
+warnings.warn(
+    "dbt-athena-community will be deprecated in favor of dbt-athena following version 1.9.x. To continue using new features, please run `pip install dbt-athena` instead.",
+    DeprecationWarning,
+)
