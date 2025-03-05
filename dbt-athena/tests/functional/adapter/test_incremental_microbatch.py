4573e01c1331c4193d7366165dca4e751974fa34
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-03-05 17:36:01 -0600

    Fix athena microbatch tests to format timestamps correctly

--- dbt-athena/tests/functional/adapter/test_incremental_microbatch.py
+++ dbt-athena/tests/functional/adapter/test_incremental_microbatch.py
@@ -6,23 +6,23 @@ from dbt.tests.adapter.incremental.test_incremental_microbatch import (
     patch_microbatch_end_time,
 )
 
+_input_model_sql = """
+{{ config(materialized='table', event_time='event_time') }}
+select 1 as id, from_iso8601_timestamp('2020-01-01T00:00:00.000000Z') as event_time
+union all
+select 2 as id, from_iso8601_timestamp('2020-01-02T00:00:00.000000Z') as event_time
+union all
+select 3 as id, from_iso8601_timestamp('2020-01-03T00:00:00.000000Z') as event_time
+"""
+
 # No requirement for a unique_id for athena microbatch!
-_microbatch_model_no_unique_id_sql = """
+_microbatch_model_sql = """
 {{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0), partition_by=['date_day']) }}
 select *, cast(event_time as date) as date_day
 from {{ ref('input_model') }}
 """
 
-microbatch_input_sql = """
-{{ config(materialized='table', event_time='event_time') }}
-select 1 as id, TIMESTAMP '2020-01-01 00:00:00-0' as event_time
-union all
-select 2 as id, TIMESTAMP '2020-01-02 00:00:00-0' as event_time
-union all
-select 3 as id, TIMESTAMP '2020-01-03 00:00:00-0' as event_time
-"""
-
-microbatch_model_no_partition_by_sql = """
+_microbatch_model_no_partition_by_sql = """
 {{ config(
     materialized='incremental',
     incremental_strategy='microbatch',
@@ -38,15 +38,19 @@ select * from {{ ref('input_model') }}
 class TestAthenaMicrobatch(BaseMicrobatch):
     @pytest.fixture(scope="class")
     def microbatch_model_sql(self) -> str:
-        return _microbatch_model_no_unique_id_sql
+        return _microbatch_model_sql
+
+    @pytest.fixture(scope="class")
+    def input_model_sql(self) -> str:
+        return _input_model_sql
 
 
 class TestAthenaMicrobatchMissingPartitionBy:
     @pytest.fixture(scope="class")
     def models(self):
         return {
-            "microbatch.sql": microbatch_model_no_partition_by_sql,
-            "input_model.sql": microbatch_input_sql,
+            "microbatch.sql": _microbatch_model_no_partition_by_sql,
+            "input_model.sql": _input_model_sql,
         }
 
     def test_execution_failure_no_partition_by(self, project):
