02b3217fbc2b1df208de7c1006ebde7c5366f61c
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-03-05 16:53:56 -0600

    Add athena microbatch test for `partition_by` error

--- dbt-athena/tests/functional/adapter/test_incremental_microbatch.py
+++ dbt-athena/tests/functional/adapter/test_incremental_microbatch.py
@@ -1,7 +1,9 @@
 import pytest
 
+from dbt.tests.util import run_dbt_and_capture
 from dbt.tests.adapter.incremental.test_incremental_microbatch import (
     BaseMicrobatch,
+    patch_microbatch_end_time,
 )
 
 # No requirement for a unique_id for athena microbatch!
@@ -11,8 +13,46 @@ select *, cast(event_time as date) as date_day
 from {{ ref('input_model') }}
 """
 
+microbatch_input_sql = """
+{{ config(materialized='table', event_time='event_time') }}
+select 1 as id, TIMESTAMP '2020-01-01 00:00:00-0' as event_time
+union all
+select 2 as id, TIMESTAMP '2020-01-02 00:00:00-0' as event_time
+union all
+select 3 as id, TIMESTAMP '2020-01-03 00:00:00-0' as event_time
+"""
 
-class TestMicrobatch(BaseMicrobatch):
+microbatch_model_no_partition_by_sql = """
+{{ config(
+    materialized='incremental',
+    incremental_strategy='microbatch',
+    event_time='event_time',
+    batch_size='day',
+    begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0)
+    )
+}}
+select * from {{ ref('input_model') }}
+"""
+
+
+class TestAthenaMicrobatch(BaseMicrobatch):
     @pytest.fixture(scope="class")
     def microbatch_model_sql(self) -> str:
         return _microbatch_model_no_unique_id_sql
+
+
+class TestAthenaMicrobatchMissingPartitionBy:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "microbatch.sql": microbatch_model_no_partition_by_sql,
+            "input_model.sql": microbatch_input_sql,
+        }
+
+    def test_execution_failure_no_partition_by(self, project):
+        with patch_microbatch_end_time("2020-01-03 13:57:00"):
+            _, stdout = run_dbt_and_capture(["run"], expect_pass=False)
+        assert (
+            "dbt-athena 'microbatch' incremental strategy requires a `partition_by` config"
+            in stdout
+        )
