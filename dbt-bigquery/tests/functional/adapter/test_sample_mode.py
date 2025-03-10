a53e8435b8db5bc0e637d8c03eb861d9f8f69a06
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-03-10 05:58:47 -0500

    Fix input_model_sql for bigquery sample mode test

--- dbt-bigquery/tests/functional/adapter/test_sample_mode.py
+++ dbt-bigquery/tests/functional/adapter/test_sample_mode.py
@@ -1,7 +1,19 @@
+import pytest
+
 from dbt.tests.adapter.sample_mode.test_sample_mode import (
     BaseSampleModeTest,
 )
 
+_input_model_sql = """
+select 1 as id, cast('2025-01-01 01:25:00-0' as datetime) as event_time
+UNION ALL
+select 2 as id, cast('2025-01-02 13:47:00-0' as datetime) as event_time
+UNION ALL
+select 3 as id, cast('2025-01-03 01:32:00-0' as datetime) as event_time
+"""
+
 
 class TestBigQuerySampleMode(BaseSampleModeTest):
-    pass
+    @pytest.fixture(scope="class")
+    def input_model_sql(self) -> str:
+        return _input_model_sql
