d150092f2ce313a21e824ed3c19a71a597d5b193
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-10 20:18:23 -0400

    provide basic case statement to re-route create table calls

--- dbt-snowflake/src/dbt/include/snowflake/macros/relations/table/create.sql
+++ dbt-snowflake/src/dbt/include/snowflake/macros/relations/table/create.sql
@@ -5,7 +5,6 @@
 {%- endif -%}
 
 {%- set catalog_name = config.get('catalog_name', default=none) -%}
-
 {%- if catalog_name is not none -%}
     {%- set catalog_integration = adapter.get_catalog_integration(catalog_name) -%}
 {%- else -%}
@@ -13,13 +12,11 @@
 {%- endif -%}
 
 {%- if language == 'sql' -%}
-    {%- if catalog_name is none -%}
+    {%- if catalog_integration is none -%}
         {{ snowflake__create_table_standard_sql(temporary, relation, compiled_code) }}
-    {% elif relation.is_iceberg_format %}
-        {{ snowflake__create_table_iceberg_managed_sql(relation, compiled_code) }}
-    {% elif catalog_integration.catalog_type in ['iceberg_rest', 'aws_glue'] %}
-        {{ snowflake__create_table_iceberg_rest_sql(relation) }}
-    {% endif %}
+    {%- else -%}
+        {%- do exceptions.raise_compiler_error("Unsupported catalog integration: " ~ catalog_name ~ " of type: " ~ catalog_integration.catalog_type) -%}
+    {%- endif -%}
 {%- elif language == 'python' -%}
     {%- if relation.is_iceberg_format -%}
         {%- do exceptions.raise_compiler_error('Iceberg is incompatible with Python models. Please use a SQL model for the iceberg format.') -%}
