9d99ec06247c438976dcb222983c985b0cc7262d
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-12 17:31:18 -0400

    update how redshift handles merge aliases for delete statements

---
+++ dbt-redshift/src/dbt/include/redshift/macros/materializations/incremental_delete_insert.sql
@@ -0,0 +1,32 @@
+{% macro default__get_delete_insert_merge_sql(target, source, unique_key, dest_columns, incremental_predicates) -%}
+    {%- set predicates = _update_predicates(incremental_predicates) -%}
+
+    {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
+
+    {% if unique_key %}
+    {% if unique_key is string %}
+    {% set unique_key = [unique_key] %}
+    {% endif %}
+
+    {%- set unique_key_str = unique_key|join(', ') -%}
+
+    delete from {{ target }}
+    where ({{ unique_key_str }}) in (
+        select distinct {{ unique_key_str }}
+        from {{ source }} as DBT_INTERNAL_SOURCE
+    )
+        {%- if incremental_predicates %}
+        {% for predicate in incremental_predicates %}
+      and {{ predicate }}
+    {% endfor %}
+    {%- endif -%};
+
+    {% endif %}
+
+    insert into {{ target }} ({{ dest_cols_csv }})
+        (
+            select {{ dest_cols_csv }}
+            from {{ source }}
+        )
+
+{%- endmacro %}
