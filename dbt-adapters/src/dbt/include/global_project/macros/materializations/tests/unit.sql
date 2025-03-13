1608b50519e66a9c898a72418e336f19786bb60c
Christophe Oudar <kayrnt@gmail.com>
2025-03-13 02:07:08 +0100

    Fix unit tests with sql_header

--- dbt-adapters/src/dbt/include/global_project/macros/materializations/tests/unit.sql
+++ dbt-adapters/src/dbt/include/global_project/macros/materializations/tests/unit.sql
@@ -13,11 +13,11 @@
 
   {% set expected_rows = config.get('expected_rows') %}
   {% set expected_sql = config.get('expected_sql') %}
-  {% set tested_expected_column_names = expected_rows[0].keys() if (expected_rows | length ) > 0 else get_columns_in_query(sql) %} %}
+  {%- set sql_header = tested_node_config.get("sql_header", "") if tested_node_config is defined else "" -%}
+  {% set tested_expected_column_names = expected_rows[0].keys() if (expected_rows | length ) > 0 else get_columns_in_query(sql, sql_header) %}
 
   {%- set target_relation = this.incorporate(type='table') -%}
   {%- set temp_relation = make_temp_relation(target_relation)-%}
-  {%- set sql_header = tested_node_config.get("sql_header", "") if tested_node_config is defined else "" -%}
   {% do run_query(get_create_table_as_sql_with_sql_header(True, temp_relation, get_empty_subquery_sql(sql), sql_header)) %}
   {%- set columns_in_relation = adapter.get_columns_in_relation(temp_relation) -%}
   {%- set column_name_to_data_types = {} -%}
