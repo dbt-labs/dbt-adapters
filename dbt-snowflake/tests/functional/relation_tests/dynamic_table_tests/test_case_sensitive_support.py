2add87c509991824b556bffcb90a2403f4f9a46d
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-14 12:09:18 -0400

    ignore the QUOTED_IDENTIFIERS_IGNORE_CASE setting during the metadata query

---
+++ dbt-snowflake/tests/functional/relation_tests/dynamic_table_tests/test_case_sensitive_support.py
@@ -0,0 +1,64 @@
+import pytest
+
+from dbt.tests.util import run_dbt
+
+from tests.functional.relation_tests.dynamic_table_tests import models
+from tests.functional.utils import update_model
+
+
+class TestCaseSensitiveSupport:
+    """
+    This test addresses https://github.com/dbt-labs/dbt-adapters/issues/917.
+
+    Setting QUOTED_IDENTIFIERS_IGNORE_CASE = TRUE overrides the quoting behavior in the metadata queries.
+    This query:
+        select
+            "name",
+            "schema_name",
+            "database_name",
+            "text",
+            "target_lag",
+            "warehouse",
+            "refresh_mode"
+        from table(result_scan(last_query_id()))
+    returns these field names:
+        NAME, SCHEMA_NAME, DATABASE_NAME, TEXT, TARGET_LAG, WAREHOUSE, REFRESH_MODE
+    when QUOTED_IDENTIFIERS_IGNORE_CASE = TRUE
+    """
+
+    @pytest.fixture(scope="class", autouse=True)
+    def seeds(self):
+        yield {"my_seed.csv": models.SEED}
+
+    @pytest.fixture(scope="class", autouse=True)
+    def models(self):
+        yield {"my_dynamic_table.sql": models.DYNAMIC_TABLE}
+
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "on_configuration_change": "apply",
+                "pre-hook": "ALTER SESSION SET QUOTED_IDENTIFIERS_IGNORE_CASE = TRUE",
+                "post-hook": "ALTER SESSION UNSET QUOTED_IDENTIFIERS_IGNORE_CASE",
+            }
+        }
+
+    @pytest.fixture(scope="class", autouse=True)
+    def setup(self, project):
+        run_dbt(["seed"])
+        yield
+        project.run_sql(f"drop schema if exists {project.test_schema} cascade")
+        project.run_sql(f"ALTER SESSION UNSET QUOTED_IDENTIFIERS_IGNORE_CASE")
+
+    def test_changes_are_applied(self, project):
+
+        # run dbt once and update the table so that the second run needs to query the metadata
+        run_dbt(["run"])
+        update_model(project, "my_dynamic_table", models.DYNAMIC_TABLE_ALTER)
+
+        # run it a second time to trigger the metadata query
+        run_dbt(["run"])
+
+        # a successful run is a pass
+        assert True
