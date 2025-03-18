ccad012200897f2b8ca8acfd0c6a54231a58867b
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-18 18:18:40 -0400

    update dbx wakeup to use documented connection

--- .github/scripts/wait_for_databricks_cluster.py
+++ .github/scripts/wait_for_databricks_cluster.py
@@ -1,14 +1,11 @@
-from base64 import standard_b64encode
 from os import getenv
 from time import sleep
 
-from pyhive import hive
-from thrift.transport.THttpClient import THttpClient
-from thrift.Thrift import TApplicationException
+from databricks import sql
 
 
 HOST = getenv("DBT_DATABRICKS_HOST_NAME")
-CLUSTER = getenv("DBT_DATABRICKS_CLUSTER_NAME")
+ENDPOINT = getenv("DBT_DATABRICKS_ENDPOINT")
 TOKEN = getenv("DBT_DATABRICKS_TOKEN")
 
 
@@ -16,24 +13,18 @@ def _wait_for_databricks_cluster() -> None:
     """
     It takes roughly 3-5 minutes for the cluster to start, to be safe we'll wait for 10 minutes
     """
-    transport_client = _transport_client()
-
-    for _ in range(20):
+    for _ in range(1):
         try:
-            hive.connect(thrift_transport=transport_client)
+            sql.connect(
+                server_hostname=HOST,
+                http_path=f"/sql/1.0/warehouses/{ENDPOINT}",
+                access_token=TOKEN,
+            )
             return
-        except TApplicationException:
+        except KeyError:
             sleep(30)
 
     raise Exception("Databricks cluster did not start in time")
-
-
-def _transport_client() -> THttpClient:
-    transport_client = THttpClient(f"https://{HOST}:443/sql/protocolv1/o/0/{CLUSTER}")
-    raw_token = f"token:{TOKEN}".encode()
-    token = standard_b64encode(raw_token).decode()
-    transport_client.setCustomHeaders({"Authorization": f"Basic {token}"})
-    return transport_client
 
 
 if __name__ == "__main__":
