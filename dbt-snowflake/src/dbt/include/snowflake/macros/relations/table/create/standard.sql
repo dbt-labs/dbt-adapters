622259a19541f92d4a81ae4f63717a835fe377bd
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-20 16:03:47 -0400

    create dedicated ddl for iceberg tables

--- dbt-snowflake/src/dbt/include/snowflake/macros/relations/table/create/standard.sql
+++ dbt-snowflake/src/dbt/include/snowflake/macros/relations/table/create/standard.sql
@@ -1,8 +1,9 @@
 {% macro snowflake__create_table_standard_sql(temporary, relation, compiled_code) -%}
-
-{%- if relation.is_iceberg_format and not adapter.behavior.enable_iceberg_materializations.no_warn %}
-{% do exceptions.raise_compiler_error('Was unable to create model as Iceberg Table Format. Please set the `enable_iceberg_materializations` behavior flag to True in your dbt_project.yml. For more information, go to https://docs.getdbt.com/reference/resource-configs/snowflake-configs#iceberg-table-format') %}
-{%- endif %}
+{#-
+    Implements CREATE TABLE and CREATE TABLE ... AS SELECT:
+    https://docs.snowflake.com/en/sql-reference/sql/create-table
+    https://docs.snowflake.com/en/sql-reference/sql/create-table#create-table-as-select-also-referred-to-as-ctas
+-#}
 
 {%- set materialization_prefix = relation.get_ddl_prefix_for_create(config.model.config, temporary) -%}
 {%- set alter_prefix = relation.get_ddl_prefix_for_alter() -%}
@@ -12,25 +13,18 @@
 {%- set copy_grants = config.get('copy_grants', default=false) -%}
 
 {%- if cluster_by_keys is not none and cluster_by_keys is string -%}
-{%- set cluster_by_keys = [cluster_by_keys] -%}
+    {%- set cluster_by_keys = [cluster_by_keys] -%}
 {%- endif -%}
 {%- if cluster_by_keys is not none -%}
-{%- set cluster_by_string = cluster_by_keys|join(", ")-%}
+    {%- set cluster_by_string = cluster_by_keys|join(", ")-%}
 {% else %}
-{%- set cluster_by_string = none -%}
+    {%- set cluster_by_string = none -%}
 {%- endif -%}
 {%- set sql_header = config.get('sql_header', none) -%}
 
 {{ sql_header if sql_header is not none }}
 
 create or replace {{ materialization_prefix }} table {{ relation }}
-    {%- if relation.is_iceberg_format %}
-      {#
-        Valid DDL in CTAS statements. Plain create statements have a different order.
-        https://docs.snowflake.com/en/sql-reference/sql/create-iceberg-table
-      #}
-      {{ relation.get_iceberg_ddl_options(config.model.config) }}
-    {%- endif -%}
 
     {%- set contract_config = config.get('contract') -%}
     {%- if contract_config.enforced -%}
