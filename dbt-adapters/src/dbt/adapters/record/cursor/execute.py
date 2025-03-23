35d811541cf5e75deb09318de7433f9fb13e2eda
Peter Allen Webb <peter.webb@dbtlabs.com>
2025-03-23 15:37:51 -0400

    Fix some small issues with record/replay serialization

--- dbt-adapters/src/dbt/adapters/record/cursor/execute.py
+++ dbt-adapters/src/dbt/adapters/record/cursor/execute.py
@@ -1,6 +1,7 @@
 import dataclasses
-from typing import Any, Iterable, Union, Mapping
+from typing import Any, Iterable, Union, Mapping, Optional
 
+from dbt.adapters.record.cursor.fetchall import CursorFetchAllResult
 from dbt_common.record import Record, Recorder
 
 
@@ -8,7 +9,26 @@ from dbt_common.record import Record, Recorder
 class CursorExecuteParams:
     connection_name: str
     operation: str
-    parameters: Union[Iterable[Any], Mapping[str, Any]]
+    parameters: Optional[Union[Iterable[Any], Mapping[str, Any]]] = None
+
+    def _to_dict(self):
+        p = self.parameters
+        if isinstance(self.parameters, dict):
+            p = {(k, CursorFetchAllResult._process_value(v)) for k, v in self.parameters.items()}
+        elif isinstance(self.parameters, list) or isinstance(self.parameters, tuple):
+            p = [CursorFetchAllResult._process_value(v) for v in self.parameters]
+
+
+        return {
+            "connection_name": self.connection_name,
+            "operation": self.operation,
+            "parameters": p
+        }
+
+    def _from_dict(cls, data):
+        # NOTE: This will be needed for replay, but is not needed at time
+        # of writing.
+        raise NotImplementedError()
 
 
 @Recorder.register_record_type
