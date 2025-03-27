2e0d711d4858cf0733121a1faef511953bed22d2
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-03-27 17:41:22 -0500

    Ensure dbt-athena microbatch models can operate on mixed hive and iceberg inputs

--- dbt-athena/tests/functional/adapter/test_incremental_microbatch.py
+++ dbt-athena/tests/functional/adapter/test_incremental_microbatch.py
@@ -6,7 +6,7 @@ from dbt.tests.adapter.incremental.test_incremental_microbatch import (
     patch_microbatch_end_time,
 )
 
-_input_model_sql = """
+_hive_input_model_sql = """
 {{ config(materialized='table', event_time='event_time') }}
 select 1 as id, CAST(from_iso8601_timestamp('2020-01-01T00:00:00.000000Z') as timestamp) as event_time
 union all
@@ -35,7 +35,20 @@ _hive_microbatch_model_sql = """
     table_type='hive'
     )
 }}
-select * from {{ ref('input_model') }}
+with
+hive_input_model as (
+    select * from {{ ref('input_model') }}
+),
+iceberg_input_model as (
+    select * from {{ ref('iceberg_input_model') }}
+)
+select
+    hive_input_model.id,
+    cast(iceberg_input_model.event_time as timestamp) as iceberg_event_time,
+    hive_input_model.event_time
+from hive_input_model
+left join iceberg_input_model
+    on hive_input_model.id = iceberg_input_model.id
 """
 
 _iceberg_microbatch_model_sql = """
@@ -49,7 +62,20 @@ _iceberg_microbatch_model_sql = """
     table_type='iceberg'
     )
 }}
-select * from {{ ref('input_model') }}
+with
+hive_input_model as (
+    select * from {{ ref('hive_input_model') }}
+),
+iceberg_input_model as (
+    select * from {{ ref('input_model') }}
+)
+select
+    iceberg_input_model.id,
+    iceberg_input_model.event_time,
+    cast(hive_input_model.event_time as timestamp(6)) as hive_event_time
+from iceberg_input_model
+left join hive_input_model
+    on iceberg_input_model.id = hive_input_model.id
 """
 
 _iceberg_microbatch_model_no_unique_key_sql = """
@@ -80,12 +106,12 @@ select * from {{ ref('input_model') }}
 
 class TestAthenaHiveMicrobatch(BaseMicrobatch):
     @pytest.fixture(scope="class")
-    def microbatch_model_sql(self) -> str:
-        return _hive_microbatch_model_sql
-
-    @pytest.fixture(scope="class")
-    def input_model_sql(self) -> str:
-        return _input_model_sql
+    def models(self):
+        return {
+            "input_model.sql": _hive_input_model_sql,
+            "iceberg_input_model.sql": _iceberg_input_model_sql,
+            "microbatch_model.sql": _hive_microbatch_model_sql,
+        }
 
 
 class TestAthenaHiveMicrobatchMissingPartitionBy:
@@ -93,7 +119,7 @@ class TestAthenaHiveMicrobatchMissingPartitionBy:
     def models(self):
         return {
             "microbatch.sql": _microbatch_model_no_partitioned_by_sql,
-            "input_model.sql": _input_model_sql,
+            "input_model.sql": _hive_input_model_sql,
         }
 
     def test_execution_failure_no_partitioned_by(self, project):
@@ -107,12 +133,12 @@ class TestAthenaHiveMicrobatchMissingPartitionBy:
 
 class TestAthenaIcebergMicrobatch(BaseMicrobatch):
     @pytest.fixture(scope="class")
-    def microbatch_model_sql(self) -> str:
-        return _iceberg_microbatch_model_sql
-
-    @pytest.fixture(scope="class")
-    def input_model_sql(self) -> str:
-        return _iceberg_input_model_sql
+    def models(self):
+        return {
+            "hive_input_model.sql": _hive_input_model_sql,
+            "input_model.sql": _iceberg_input_model_sql,
+            "microbatch_model.sql": _iceberg_microbatch_model_sql,
+        }
 
 
 class TestAthenaIcebergMicrobatchMissingUniqueKey:
