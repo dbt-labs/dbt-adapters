d32d08a67c0b895e7888fa50199085eee3ab4e76
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-28 19:33:36 -0400

    allow users to pass None for base location parts and still use defaults

--- dbt-snowflake/tests/unit/test_iceberg_location.py
+++ dbt-snowflake/tests/unit/test_iceberg_location.py
@@ -1,9 +1,7 @@
-from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional
+from copy import deepcopy
 
 import pytest
 
-from dbt.adapters.contracts.relation import MaterializationConfig, RelationConfig
 from dbt.adapters.snowflake.catalogs import (
     IcebergManagedCatalogIntegration,
     DEFAULT_ICEBERG_CATALOG_INTEGRATION,
@@ -15,34 +13,28 @@ def fake_integration() -> IcebergManagedCatalogIntegration:
     return IcebergManagedCatalogIntegration(DEFAULT_ICEBERG_CATALOG_INTEGRATION)
 
 
-@dataclass
-class FakeMaterializationConfig:
-    extra: Dict[str, Any]
-
-
-@dataclass
-class FakeRelationConfig:
-    resource_type: str = ""
-    name: str = ""
-    description: str = ""
-    database: str = "my_database"
-    schema: str = "my_schema"
-    identifier: str = "my_table"
-    compiled_code: Optional[str] = None
-    meta: Dict[str, Any] = field(default_factory=dict)
-    tags: List[str] = field(default_factory=list)
-    quoting_dict: Dict[str, bool] = field(default_factory=dict)
-    config: Optional[MaterializationConfig] = field(default_factory=FakeMaterializationConfig)
-    catalog: Optional[str] = "snowflake"
-
-    external_volume: Optional[str] = "s3_iceberg_snow"
-    base_location_root: Optional[str] = None
-    base_location_subpath: Optional[str] = None
+model_base = {
+    "database": "my_database",
+    "schema": "my_schema",
+    "alias": "my_table",
+    "config": {
+        "catalog": "snowflake",
+        "external_volume": "s3_iceberg_snow",
+    },
+}
 
 
 @pytest.mark.parametrize(
     "config,expected",
     [
+        (
+            {},
+            "_dbt/my_schema/my_table",
+        ),
+        (
+            {"base_location_root": None, "base_location_subpath": None},
+            "_dbt/my_schema/my_table",
+        ),
         (
             {"base_location_root": "root_path", "base_location_subpath": "subpath"},
             "root_path/my_schema/my_table/subpath",
@@ -55,15 +47,11 @@ class FakeRelationConfig:
             {"base_location_root": "root_path"},
             "root_path/my_schema/my_table",
         ),
-        (
-            {},
-            "_dbt/my_schema/my_table",
-        ),
     ],
 )
 def test_iceberg_base_location_managed(fake_integration, config, expected):
     """Test when base_location_root and base_location_subpath are provided"""
-    materialization_config = FakeMaterializationConfig(extra=config)
-    relation_config = FakeRelationConfig(config=materialization_config)
-    relation = fake_integration.build_relation(relation_config)
+    model = deepcopy(model_base)
+    model["config"].update(config)
+    relation = fake_integration.build_relation(model)
     assert relation.base_location == expected
