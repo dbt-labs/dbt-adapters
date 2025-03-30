25fc863ba0904ebd44a59df6ada6ec76a40f0274
Mike Alfare <mike.alfare@dbtlabs.com>
2025-03-30 14:57:45 -0400

    fix typo in variable ref

--- dbt-snowflake/src/dbt/include/snowflake/macros/relations/dynamic_table/replace.sql
+++ dbt-snowflake/src/dbt/include/snowflake/macros/relations/dynamic_table/replace.sql
@@ -16,10 +16,10 @@
     {%- set dynamic_table = relation.from_config(config.model) -%}
     {%- set catalog_integration = adapter.get_catalog_integration_from_model(config.model) -%}
 
-    {%- if catalog is none -%}
+    {%- if catalog_integration is none -%}
         {{ snowflake__replace_dynamic_table_standard_sql(dynamic_table, relation, sql) }}
     {%- elif catalog_integration.catalog_type == 'iceberg_managed' -%}
-        {{ snowflake__replace_dynamic_table_iceberg_managed_sql(dynamic_table, relation, catalog, sql) }}
+        {{ snowflake__replace_dynamic_table_iceberg_managed_sql(dynamic_table, relation, sql) }}
     {%- endif -%}
 
 {%- endmacro %}
