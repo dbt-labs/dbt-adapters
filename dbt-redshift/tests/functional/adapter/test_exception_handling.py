264fb90f3bc23530120a90f1adfbedc08f061913
Colin <colin.rogers@dbtlabs.com>
2025-04-28 13:20:31 -0700

    clean up test

--- dbt-redshift/tests/functional/adapter/test_exception_handling.py
+++ dbt-redshift/tests/functional/adapter/test_exception_handling.py
@@ -1,23 +1,7 @@
 import pytest
 from dbt.tests.util import run_dbt_and_capture
 
-
-class TestRetryOnRelationOidNotFound:
-    """
-    test based on bug report: https://github.com/dbt-labs/dbt-adapters/issues/642
-    """
-
-    @pytest.fixture(scope="class")
-    def profiles_config_update(self, dbt_profile_target):
-        outputs = {"default": dbt_profile_target}
-        outputs["default"]["retries"] = 5
-        outputs["default"]["threads"] = 4
-        return outputs
-
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "test_table.sql": """
+_TEST_MODEL = """
                 {{
                   config(
                     materialized = 'table',
@@ -25,12 +9,9 @@ class TestRetryOnRelationOidNotFound:
                 }}
                 select 1 as test_col
             """
-        }
 
-    @pytest.fixture(scope="class")
-    def macros(self):
-        return {
-            "macro.sql": """
+_MACROS = {
+    "macro.sql": """
             {% macro transaction_macro() %}
                 {% if execute %}
                     begin transaction;
@@ -48,7 +29,31 @@ class TestRetryOnRelationOidNotFound:
                 {% endif %}
             {% endmacro %}
             """
-        }
+}
+
+
+class TestRetryOnRelationOidNotFound:
+    """
+    test based on bug report: https://github.com/dbt-labs/dbt-adapters/issues/642
+
+    on-run-end macro is called twice because we will run the hooks concurrently ensuring we hit the exception
+
+    """
+
+    @pytest.fixture(scope="class")
+    def profiles_config_update(self, dbt_profile_target):
+        outputs = {"default": dbt_profile_target}
+        outputs["default"]["retries"] = 5
+        outputs["default"]["threads"] = 4
+        return outputs
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"test_table.sql": _TEST_MODEL}
+
+    @pytest.fixture(scope="class")
+    def macros(self):
+        return _MACROS
 
     @pytest.fixture(scope="class")
     def project_config_update(self):
@@ -61,6 +66,5 @@ class TestRetryOnRelationOidNotFound:
 
     def test_retry_on_relation_oid_not_found(self, project):
         result, stdout = run_dbt_and_capture(["run", "--log-level=debug"])
-        print(stdout)
         assert "could not open relation with OID" in stdout
         assert "Redshift adapter: Retrying query due to error: Database Error" in stdout
