61fcbd0bbeac841db3c9f80750f3fef6b9d9e8a2
Colin <colin.rogers@dbtlabs.com>
2025-06-02 09:20:11 -0700

    fix bigquery file_format

--- dbt-bigquery/src/dbt/adapters/bigquery/constants.py
+++ dbt-bigquery/src/dbt/adapters/bigquery/constants.py
@@ -1,19 +1,14 @@
 from types import SimpleNamespace
 
-
 ADAPTER_TYPE = "bigquery"
-
 
 INFO_SCHEMA_TABLE_FORMAT = "default"
 ICEBERG_TABLE_FORMAT = "iceberg"
 
-
 INFO_SCHEMA_FILE_FORMAT = "default"
 PARQUET_FILE_FORMAT = "parquet"
 
-
 GENERIC_CATALOG_TYPE = "generic"
-
 
 DEFAULT_INFO_SCHEMA_CATALOG = SimpleNamespace(
     name="info_schema",
@@ -21,9 +16,7 @@ DEFAULT_INFO_SCHEMA_CATALOG = SimpleNamespace(
     catalog_type=GENERIC_CATALOG_TYPE,
     table_format=INFO_SCHEMA_TABLE_FORMAT,
     external_volume=None,
-    adapter_properties={
-        "file_format": INFO_SCHEMA_FILE_FORMAT,
-    },
+    file_format=INFO_SCHEMA_FILE_FORMAT,
 )
 DEFAULT_ICEBERG_CATALOG = SimpleNamespace(
     name="managed_iceberg",
@@ -31,7 +24,5 @@ DEFAULT_ICEBERG_CATALOG = SimpleNamespace(
     catalog_type=GENERIC_CATALOG_TYPE,
     table_format=ICEBERG_TABLE_FORMAT,
     external_volume=None,
-    adapter_properties={
-        "file_format": PARQUET_FILE_FORMAT,
-    },
+    file_format=PARQUET_FILE_FORMAT,
 )
