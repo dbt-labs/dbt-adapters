18e00de7ce0ee5c5614fbe243b7383135738f387
Yutsing Liu <yutsing.liu@snowflake.com>
2025-08-13 16:25:54 -0700

    Add Materialization

---
+++ dbt-snowflake/src/dbt/include/snowflake/macros/materializations/semantic_view.sql.sql
@@ -0,0 +1,15 @@
+{% materialization semantic_view, adapter='snowflake' -%}
+
+    {% set original_query_tag = set_query_tag() %}
+    {% set to_return = snowflake__create_or_replace_semantic_view() %}
+
+    {% set target_relation = this.incorporate(type='semantic_view') %}
+
+    -- Requires COMMENT ON SEMANTIC VIEW support
+    -- {% do persist_docs(target_relation, model, for_columns=false) %}
+
+    {% do unset_query_tag(original_query_tag) %}
+
+    {% do return(to_return) %}
+
+{%- endmaterialization %}
