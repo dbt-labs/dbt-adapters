dfe9db560f957cf8c7fe2d1eb482b987a33fa260
Yutsing Liu <yutsing.liu@snowflake.com>
2025-08-13 21:05:59 -0700

    Add relations

---
+++ dbt-snowflake/src/dbt/include/snowflake/macros/relations/semantic_view/create.sql
@@ -0,0 +1,53 @@
+{% macro snowflake__get_create_semantic_view_sql(relation, sql) -%}
+{#-
+--  Produce DDL that creates a semantic view
+--
+--  Args:
+--  - relation: Union[SnowflakeRelation, str]
+--      - SnowflakeRelation - required for relation.render()
+--      - str - is already the rendered relation name
+--  - sql: str - the code defining the model
+--  Returns:
+--      A valid DDL statement which will result in a new semantic view.
+-#}
+
+  create semantic view {{ relation }}
+  {{ sql }}
+
+{%- endmacro %}
+
+
+{% macro snowflake__create_or_replace_semantic_view() %}
+  {%- set identifier = model['alias'] -%}
+
+  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
+  {%- set exists_as_view = (old_relation is not none and old_relation.is_semantic_view) -%}
+
+  {%- set target_relation = api.Relation.create(
+      identifier=identifier, schema=schema, database=database,
+      type='semantic_view') -%}
+  {% set grant_config = config.get('grants') %}
+
+  {{ run_hooks(pre_hooks) }}
+
+  -- If there's a table with the same name and we weren't told to full refresh,
+  -- that's an error. If we were told to full refresh, drop it. This behavior differs
+  -- for Snowflake and BigQuery, so multiple dispatch is used.
+  {%- if old_relation is not none and not old_relation.is_semantic_view -%}
+    {{ handle_existing_table(should_full_refresh(), old_relation) }}
+  {%- endif -%}
+
+  -- build model
+  {% call statement('main') -%}
+    {{ snowflake__get_create_semantic_view_sql(target_relation, sql) }}
+  {%- endcall %}
+
+  -- TODO: Properly handle hierarchy of specification
+  -- {% set should_revoke = should_revoke(exists_as_view, full_refresh_mode=True) %}
+  -- {% do apply_grants(target_relation, grant_config, should_revoke=should_revoke) %}
+
+  {{ run_hooks(post_hooks) }}
+
+  {{ return({'relations': [target_relation]}) }}
+
+{% endmacro %}
