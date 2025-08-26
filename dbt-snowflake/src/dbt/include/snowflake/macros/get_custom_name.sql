63df5075d35da2b74e4b6540335a78ad58810730
Colin <colin.rogers@dbtlabs.com>
2025-08-26 11:31:37 -0700

    remove unneeded return

--- dbt-snowflake/src/dbt/include/snowflake/macros/get_custom_name.sql
+++ dbt-snowflake/src/dbt/include/snowflake/macros/get_custom_name.sql
@@ -10,9 +10,9 @@
             and catalog_relation|attr('catalog_linked_database')-%}
             {{ return(catalog_relation.catalog_linked_database) }}
         {%- else -%}
-            {{ return(target.database) }}
+            {{ target.database }}
         {%- endif -%}
     {%- else -%}
-       {{ return(custom_database_name) }}
+       {{ custom_database_name }}
     {%- endif -%}
 {%- endmacro %}
