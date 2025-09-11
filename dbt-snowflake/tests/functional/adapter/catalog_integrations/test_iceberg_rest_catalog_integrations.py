b0223a88d67012bcc4c6cce5449c4fe10c6ed198
Colin Rogers <111200756+colin-rogers-dbt@users.noreply.github.com>
2025-09-11 11:55:02 -0700

    update incremental sql to support iceberg REST w CLD (#1287)
    Co-authored-by: Zeeshan Qureshi <zee@zqureshi.in>
    Co-authored-by: tnk-ysk <61967017+tnk-ysk@users.noreply.github.com>
    Co-authored-by: Mila Page <67295367+VersusFacit@users.noreply.github.com>
    Co-authored-by: sandeepmullangi2 <70845449+sandeepmullangi2@users.noreply.github.com>
    Co-authored-by: Daisuke Taniwaki <daisuketaniwaki@gmail.com>
    Co-authored-by: Doug Beatty <44704949+dbeatty10@users.noreply.github.com>

--- dbt-snowflake/tests/functional/adapter/catalog_integrations/test_iceberg_rest_catalog_integrations.py
+++ dbt-snowflake/tests/functional/adapter/catalog_integrations/test_iceberg_rest_catalog_integrations.py
@@ -17,6 +17,38 @@ MODEL__ICEBERG_TABLE_WITH_CATALOG_CONFIG = """
                             select 1 as id
                             """
 
+MODEL__INCREMENTAL_ICEBERG_REST = """
+{{
+  config(
+    materialized='incremental',
+    catalog_name='basic_iceberg_rest_catalog',
+    incremental_strategy='merge',
+    unique_key="id",
+  )
+}}
+select * from {{ ref('basic_iceberg_table') }}
+
+{% if is_incremental() %}
+where id > 2
+{% endif %}
+"""
+
+MODEL__INCREMENTAL_ICEBERG_REST_INSERT_OVERWRITE = """
+{{
+  config(
+    materialized='incremental',
+    catalog_name='basic_iceberg_rest_catalog',
+    incremental_strategy='insert_overwrite',
+    unique_key="id",
+  )
+}}
+select * from {{ ref('basic_iceberg_table') }}
+
+{% if is_incremental() %}
+where id > 2
+{% endif %}
+"""
+
 
 class TestSnowflakeIcebergRestCatalogIntegration(BaseCatalogIntegrationValidation):
 
@@ -62,10 +94,12 @@ class TestSnowflakeIcebergRestCatalogIntegration(BaseCatalogIntegrationValidatio
             "models": {
                 "basic_iceberg_table.sql": MODEL__BASIC_ICEBERG_TABLE,
                 "iceberg_table_with_catalog_config.sql": MODEL__ICEBERG_TABLE_WITH_CATALOG_CONFIG,
+                "incremental_iceberg_rest.sql": MODEL__INCREMENTAL_ICEBERG_REST,
+                "incremental_iceberg_rest_insert_overwrite.sql": MODEL__INCREMENTAL_ICEBERG_REST_INSERT_OVERWRITE,
             }
         }
 
     def test_basic_iceberg_rest_catalog_integration(self, project):
         result = run_dbt(["run"])
-        assert len(result) == 2
+        assert len(result) == 4
         run_dbt(["run"])
