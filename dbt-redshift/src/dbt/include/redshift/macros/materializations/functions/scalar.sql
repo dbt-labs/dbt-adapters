85295682e9d4857d3b04484a604dae62f1b76351
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-09-29 18:08:53 -0500

    More whitespace?

--- dbt-redshift/src/dbt/include/redshift/macros/materializations/functions/scalar.sql
+++ dbt-redshift/src/dbt/include/redshift/macros/materializations/functions/scalar.sql
@@ -1,8 +1,9 @@
 {% macro redshift__scalar_function_create_replace_signature_sql(target_relation) %}
     CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
         RETURNS {{ model.return_type.type }}
+
     {#-- TODO: Stop defaulting to VOLATILE once we have a way to set the volatility --#}
-     VOLATILE
+    VOLATILE
     AS
 {% endmacro %}
 
