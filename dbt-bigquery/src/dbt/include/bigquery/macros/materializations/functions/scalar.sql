dba442d0b23f987d0384853db9f5b2f27818ab7f
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-09-29 14:40:35 -0500

    Add support for scalar functions to dbt-bigquery

---
+++ dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
@@ -0,0 +1,12 @@
+{% macro bigquery__scalar_function_create_replace_signature_sql(target_relation) %}
+    CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
+    RETURNS {{ model.return_type.type }}
+    LANGUAGE SQL
+    AS
+{% endmacro %}
+
+{% macro bigquery__scalar_function_body_sql() %}
+    (
+       {{ model.compiled_code }}
+    )
+{% endmacro %}
