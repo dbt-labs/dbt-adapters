fa70d59e412120242b0aa30173f841e632a1e5e5
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-09-29 17:38:18 -0500

    Ensure there is a space character before `VOLATILE` in redshift scalar function macro

--- dbt-redshift/src/dbt/include/redshift/macros/materializations/functions/scalar.sql
+++ dbt-redshift/src/dbt/include/redshift/macros/materializations/functions/scalar.sql
@@ -2,7 +2,7 @@
     CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
         RETURNS {{ model.return_type.type }}
     {#-- TODO: Stop defaulting to VOLATILE once we have a way to set the volatility --#}
-    VOLATILE
+     VOLATILE
     AS
 {% endmacro %}
 
