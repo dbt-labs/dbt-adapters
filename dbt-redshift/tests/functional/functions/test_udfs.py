2fd2e8643cde3dd6bcf7287423041765601da574
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-09-29 17:04:40 -0500

    Drop later: Debugging redshift via tests run in github actions

--- dbt-redshift/tests/functional/functions/test_udfs.py
+++ dbt-redshift/tests/functional/functions/test_udfs.py
@@ -1,3 +1,6 @@
+from dbt.artifacts.schemas.results import RunStatus
+from dbt.contracts.graph.nodes import FunctionNode
+from dbt.tests.util import run_dbt
 import pytest
 from dbt.tests.adapter.functions.files import MY_UDF_YML
 from dbt.tests.adapter.functions.test_udfs import UDFsBasic
@@ -13,4 +16,19 @@ class TestRedshiftUDFs(UDFsBasic):
             "price_for_xlarge.yml": MY_UDF_YML,
         }
 
-    pass
+    def test_udfs(self, project):
+        result = run_dbt(["build", "--debug"])
+
+        assert len(result.results) == 1
+        node_result = result.results[0]
+        assert node_result.status == RunStatus.Success
+        node = node_result.node
+        assert isinstance(node, FunctionNode)
+        assert node_result.node.name == "price_for_xlarge"
+
+        # TODO: use `function` instead of `ref`
+        result = run_dbt(["show", "--inline", "SELECT {{ function('price_for_xlarge') }}(100)"])
+        assert len(result.results) == 1
+        # The result should have an agate table with one row and one column (and thus only one value, which is our inline selection)
+        select_value = int(result.results[0].agate_table.rows[0].values()[0])
+        assert select_value == 200  # the UDF should return 2x the input value (100 * 2 = 200)
