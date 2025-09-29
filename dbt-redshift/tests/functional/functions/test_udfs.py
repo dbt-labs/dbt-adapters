e621c488c45907e807586237f8b562ccd73c0ed6
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-09-29 13:57:45 -0500

    Begin testing scalar functions for dbt-redshift

---
+++ dbt-redshift/tests/functional/functions/test_udfs.py
@@ -0,0 +1,16 @@
+import pytest
+from dbt.tests.adapter.functions.files import MY_UDF_YML
+from dbt.tests.adapter.functions.test_udfs import UDFsBasic
+from tests.functional.functions.files import MY_UDF_SQL
+
+
+class TestRedshiftUDFs(UDFsBasic):
+
+    @pytest.fixture(scope="class")
+    def functions(self):
+        return {
+            "price_for_xlarge.sql": MY_UDF_SQL,
+            "price_for_xlarge.yml": MY_UDF_YML,
+        }
+
+    pass
