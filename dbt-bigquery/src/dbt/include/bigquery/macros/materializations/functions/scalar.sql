ff04e52d5f023408ccad0d2db5206647599e7325
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-09-29 16:03:04 -0500

    Bigquery disallows language specification for functions when creating a SQL function

--- dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
+++ dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
@@ -1,7 +1,6 @@
 {% macro bigquery__scalar_function_create_replace_signature_sql(target_relation) %}
     CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
     RETURNS {{ model.return_type.type }}
-    LANGUAGE SQL
     AS
 {% endmacro %}
 
