d19556de453d0a98fc5b918335b66dfbffb796f6
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-10-02 11:51:18 -0500

    Fix references to function properties in function macros in dbt-adapters

--- dbt-adapters/src/dbt/include/global_project/macros/materializations/functions/scalar.sql
+++ dbt-adapters/src/dbt/include/global_project/macros/materializations/functions/scalar.sql
@@ -12,7 +12,9 @@
 {% endmacro %}
 
 {% macro default__scalar_function_create_replace_signature_sql(target_relation) %}
-    CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}}) RETURNS {{ model.return_type.type }} AS
+    CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
+    RETURNS {{ model.returns.data_type }}
+    AS
 {% endmacro %}
 
 {% macro formatted_scalar_function_args_sql() %}
@@ -22,7 +24,7 @@
 {% macro default__formatted_scalar_function_args_sql() %}
     {% set args = [] %}
     {% for arg in model.arguments -%}
-        {%- do args.append(arg.name ~ ' ' ~ arg.type) -%}
+        {%- do args.append(arg.name ~ ' ' ~ arg.data_type) -%}
     {%- endfor %}
     {{ args | join(', ') }}
 {% endmacro %}
