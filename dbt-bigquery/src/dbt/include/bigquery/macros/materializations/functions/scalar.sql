550d9dd14a2146c25b8e7156067c00b36ddac376
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-10-02 12:10:39 -0500

    Fix references to function properties in function macros for dbt-bigquery

--- dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
+++ dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
@@ -1,6 +1,6 @@
 {% macro bigquery__scalar_function_create_replace_signature_sql(target_relation) %}
     CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
-    RETURNS {{ model.return_type.type }}
+    RETURNS {{ model.returns.data_type }}
     AS
 {% endmacro %}
 
