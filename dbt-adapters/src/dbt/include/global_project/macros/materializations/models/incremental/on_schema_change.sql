ad1f29092833ae98772e47259f72eafeffdbdf94
Christophe Oudar <kayrnt@gmail.com>
2025-10-14 19:43:06 +0200

    review changes

--- dbt-adapters/src/dbt/include/global_project/macros/materializations/models/incremental/on_schema_change.sql
+++ dbt-adapters/src/dbt/include/global_project/macros/materializations/models/incremental/on_schema_change.sql
@@ -17,6 +17,11 @@
 
 
 {% macro check_for_schema_changes(source_relation, target_relation) %}
+  {{ return(adapter.dispatch('check_for_schema_changes', 'dbt')(source_relation, target_relation)) }}
+{% endmacro %}
+
+
+{% macro default__check_for_schema_changes(source_relation, target_relation) %}
 
   {% set schema_changed = False %}
 
@@ -107,7 +112,7 @@
 
     {% else %}
 
-      {% set schema_changes_dict = adapter.dispatch('check_for_schema_changes', 'dbt')(source_relation, target_relation) %}
+      {% set schema_changes_dict = check_for_schema_changes(source_relation, target_relation) %}
 
       {% if schema_changes_dict['schema_changed'] %}
 
