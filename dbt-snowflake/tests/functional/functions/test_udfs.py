fe152445dbb073b43fda73abc0911edef297d196
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-10-29 16:28:50 -0500

    Begin testing snowflake udf volatility support

--- dbt-snowflake/tests/functional/functions/test_udfs.py
+++ dbt-snowflake/tests/functional/functions/test_udfs.py
@@ -1,6 +1,13 @@
 import pytest
+from dbt.contracts.results import RunStatus
 from dbt.tests.adapter.functions.files import MY_UDF_YML
-from dbt.tests.adapter.functions.test_udfs import UDFsBasic
+from dbt.tests.adapter.functions.test_udfs import (
+    UDFsBasic,
+    DeterministicUDF,
+    StableUDF,
+    NonDeterministicUDF,
+)
+from dbt.tests.util import run_dbt
 from tests.functional.functions.files import MY_UDF_SQL
 
 
@@ -13,4 +20,23 @@ class TestSnowflakeUDFs(UDFsBasic):
             "price_for_xlarge.yml": MY_UDF_YML,
         }
 
+
+class TestSnowflakeDeterministicUDFs(DeterministicUDF):
+    pass
+
+
+class TestSnowflakeStableUDFs(StableUDF):
+    def test_udfs(self, project, sql_event_catcher):
+        result = run_dbt(
+            ["build", "--debug"], expect_pass=False, callbacks=[sql_event_catcher.catch]
+        )
+        assert len(result.results) == 1
+        assert result.results[0].status == RunStatus.Error
+        assert (
+            "`Stable` function volatility is not supported for Snowflake"
+            in result.results[0].message
+        )
+
+
+class TestSnowflakeNonDeterministicUDFs(NonDeterministicUDF):
     pass
