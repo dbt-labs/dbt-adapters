c897e65719b6fd9862475b98c7cb18b6141b4ca6
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-10-30 12:14:56 -0500

    Update dbt-bigquery to raise warning when any volatility is present for functions

--- dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
+++ dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
@@ -1,6 +1,7 @@
 {% macro bigquery__scalar_function_create_replace_signature_sql(target_relation) %}
     CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
     RETURNS {{ model.returns.data_type }}
+    {{ scalar_function_volatility_sql() }}
     AS
 {% endmacro %}
 
@@ -8,4 +9,11 @@
     (
        {{ model.compiled_code }}
     )
+{% endmacro %}
+
+{% macro bigquery__scalar_function_volatility_sql() %}
+    {% set volatility = model.config.get('volatility') %}
+    {% if volatility is not None %}
+        {% do exceptions.warn("`" ~ volatility ~ "` function volatility is not supported by BigQuery, and will be ignored") %}
+    {% endif %}
 {% endmacro %}
