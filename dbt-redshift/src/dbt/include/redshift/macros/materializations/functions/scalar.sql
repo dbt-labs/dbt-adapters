9125f897edc0fe6336225842224865f5df57c6d8
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-10-30 13:59:20 -0500

    Ensure redshift volatility is being templated instead of hard coded

--- dbt-redshift/src/dbt/include/redshift/macros/materializations/functions/scalar.sql
+++ dbt-redshift/src/dbt/include/redshift/macros/materializations/functions/scalar.sql
@@ -1,9 +1,7 @@
 {% macro redshift__scalar_function_create_replace_signature_sql(target_relation) %}
     CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
         RETURNS {{ model.returns.data_type }}
-    {# TODO: Stop defaulting to VOLATILE once we have a way to set the volatility #}
-    {# We set a default here because redshift requires a volatility to be set #}
-    VOLATILE
+    {{ scalar_function_volatility_sql() }}
     AS
 {% endmacro %}
 
