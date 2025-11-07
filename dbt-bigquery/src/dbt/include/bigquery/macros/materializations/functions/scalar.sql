6a071a43eef6eda6fa558113cb5fe59ed9bb9a5c
Quigley Malcolm <QMalcolm@users.noreply.github.com>
2025-11-06 18:02:00 -0600

    Python UDF support for Snowflake and BigQuery (#1425)

--- dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
+++ dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/scalar.sql
@@ -17,3 +17,23 @@
         {% do unsupported_volatility_warning(volatility) %}
     {% endif %}
 {% endmacro %}
+
+{% macro bigquery__scalar_function_create_replace_signature_python(target_relation) %}
+    CREATE OR REPLACE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
+    RETURNS {{ model.returns.data_type }}
+    LANGUAGE python
+    OPTIONS(runtime_version = "{{ 'python-' ~ model.config.get('runtime_version') }}", entry_point = "{{ model.config.get('entry_point') }}")
+    {{ scalar_function_volatility_sql() }}
+    AS
+{% endmacro %}
+
+{% macro bigquery__get_scalar_function_body_python() %}
+    r'''
+{{ model.compiled_code }}
+    '''
+{% endmacro %}
+
+{% macro bigquery__scalar_function_python(target_relation) %}
+    {{ bigquery__scalar_function_create_replace_signature_python(target_relation) }}
+    {{ bigquery__get_scalar_function_body_python() }}
+{% endmacro %}
