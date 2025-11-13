7b3821a78a470a6866dd8b6ee1ff031e20e3c3dc
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2025-11-13 12:30:21 -0600

    Add default aggregate function signature handling macro to dbt-adapters package

--- dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/aggregate.sql
+++ dbt-bigquery/src/dbt/include/bigquery/macros/materializations/functions/aggregate.sql
@@ -1,12 +1,5 @@
-{% macro bigquery__get_aggregate_function_create_replace_signature_sql(target_relation) %}
-    CREATE OR REPLACE AGGREGATE FUNCTION {{ target_relation.render() }} ({{ formatted_scalar_function_args_sql()}})
-    RETURNS {{ model.returns.data_type }}
-    {{ scalar_function_volatility_sql() }}
-    AS
-{% endmacro %}
-
 {% macro bigquery__aggregate_function_sql(target_relation) %}
-    {{ bigquery__get_aggregate_function_create_replace_signature_sql(target_relation) }}
+    {{ get_aggregate_function_create_replace_signature(target_relation) }}
     {# conviently we can reuse the sql scalar function body #}
     {{ scalar_function_body_sql() }}
 {% endmacro %}
