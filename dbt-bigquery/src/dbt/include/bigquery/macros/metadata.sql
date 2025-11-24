61cbea004c49fe320ffdd2d2fb9a05bd3566e6f8
MichelleArk <michelle.ark@dbtlabs.com>
2025-11-24 15:44:49 -0500

    make use of legacy __TABLES__ metadata

--- dbt-bigquery/src/dbt/include/bigquery/macros/metadata.sql
+++ dbt-bigquery/src/dbt/include/bigquery/macros/metadata.sql
@@ -1,26 +1,25 @@
 {% macro bigquery__get_relation_last_modified(information_schema, relations) -%}
+  {% set unique_schemas = (relations | map(attribute='schema')) | list | unique %}
 
-  {% set info_schema_relation = relations[0].information_schema("OBJECT_PRIVILEGES") %}
-  {% set unique_schemas = uniq(relations | map(attribute='schema')) %}
+  {% set table_ids = (relations | map(attribute='identifier')) | list %}
+  {% set table_ids_formatted = [] %}
+  {% for table_id in table_ids %}
+    {% do table_ids_formatted.append("'" + table_id + "'") %}
+  {% endfor %}
+
 
   {%- call statement('last_modified', fetch_result=True) -%}
-        select table_schema as schema,
-        table_name as identifier,
-        storage_last_modified_time as last_modified,
-               {{ current_timestamp() }} as snapshotted_at
-        from {{ info_schema_relation }}
-        where (upper(table_schema) IN (
-            {%- for unique_schema in unique_schemas -%}
-            upper('{{ unique_schema }}'){%- if not loop.last %},{% endif -%}
-            {%- endfor -%}
-            )
-        )
-        and (
-          {%- for relation in relations -%}
-            (upper(table_schema) = upper('{{ relation.schema }}') and
-             upper(table_name) = upper('{{ relation.identifier }}')){%- if not loop.last %} or {% endif -%}
-          {%- endfor -%}
-        )
+  {%- for unique_schema in unique_schemas -%}
+        select
+            dataset_id AS schema,
+            table_id AS identifier,
+            TIMESTAMP_MILLIS(last_modified_time) AS last_modified,
+            {{ current_timestamp() }} as snapshotted_at
+        from {{ unique_schema }}.__TABLES__
+        where (table_id in ({{ table_ids_formatted | join(',') }}))
+
+        {{ 'union all\n' if not loop.last else "" }}
+  {%- endfor -%}
   {%- endcall -%}
 
   {{ return(load_result('last_modified')) }}
