1a1ebcd5f3007af2c1e9669a57ca414719165647
MichelleArk <michelle.ark@dbtlabs.com>
2025-11-26 16:26:23 -0500

    use TABLE_STORAGE information schema for batched source metadata retrieval

--- dbt-bigquery/src/dbt/include/bigquery/macros/metadata.sql
+++ dbt-bigquery/src/dbt/include/bigquery/macros/metadata.sql
@@ -1,25 +1,28 @@
 {% macro bigquery__get_relation_last_modified(information_schema, relations) -%}
+
   {% set unique_schemas = (relations | map(attribute='schema')) | list | unique %}
 
-  {% set table_ids = (relations | map(attribute='identifier')) | list %}
-  {% set table_ids_formatted = [] %}
-  {% for table_id in table_ids %}
-    {% do table_ids_formatted.append("'" + table_id + "'") %}
-  {% endfor %}
-
+  {% set location = adapter.get_dataset_location(relations[0]) %}
+  {% set information_schema = relations[0].incorporate(location=location).information_schema("TABLE_STORAGE") %}
 
   {%- call statement('last_modified', fetch_result=True) -%}
-  {%- for unique_schema in unique_schemas -%}
-        select
-            dataset_id AS schema,
-            table_id AS identifier,
-            TIMESTAMP_MILLIS(last_modified_time) AS last_modified,
-            {{ current_timestamp() }} as snapshotted_at
-        from {{ unique_schema }}.__TABLES__
-        where (table_id in ({{ table_ids_formatted | join(',') }}))
-
-        {{ 'union all\n' if not loop.last else "" }}
-  {%- endfor -%}
+        select table_schema as schema,
+        table_name as identifier,
+        storage_last_modified_time as last_modified,
+               {{ current_timestamp() }} as snapshotted_at
+        from {{ information_schema }}
+        where (upper(table_schema) IN (
+            {%- for unique_schema in unique_schemas -%}
+            upper('{{ unique_schema }}'){%- if not loop.last %},{% endif -%}
+            {%- endfor -%}
+            )
+        )
+        and (
+          {%- for relation in relations -%}
+            (upper(table_schema) = upper('{{ relation.schema }}') and
+             upper(table_name) = upper('{{ relation.identifier }}')){%- if not loop.last %} or {% endif -%}
+          {%- endfor -%}
+        )
   {%- endcall -%}
 
   {{ return(load_result('last_modified')) }}
