860f79b34282e943846e5debae8f585fe5e77e1d
Theo Tolv <tolv@amazon.com>
2026-02-09 16:25:34 +0100

    Replace PyAthena with direct boto3 calls
    This implementation can run all unit and most functional tests, but lacks many aspects of the previous implementation. Fixes for the gaps will be added as follow ups.

--- dbt-athena/src/dbt/adapters/athena/impl.py
+++ dbt-athena/src/dbt/adapters/athena/impl.py
@@ -27,12 +27,11 @@ from mypy_boto3_glue.type_defs import (
     TableTypeDef,
     TableVersionTypeDef,
 )
-from pyathena.error import OperationalError
 
 from dbt.adapters.athena import AthenaConnectionManager
 from dbt.adapters.athena.column import AthenaColumn
 from dbt.adapters.athena.config import get_boto3_config
-from dbt.adapters.athena.connections import AthenaCursor
+from dbt.adapters.athena.connections import AthenaCursor, AthenaError
 from dbt.adapters.athena.constants import LOGGER
 from dbt.adapters.athena.exceptions import (
     S3LocationException,
@@ -1395,7 +1394,7 @@ class AthenaAdapter(SQLAdapter):
     def run_query_with_partitions_limit_catching(self, sql: str) -> str:
         try:
             cursor = self._run_query(sql, catch_partitions_limit=True)
-        except OperationalError as e:
+        except AthenaError as e:
             if "TOO_MANY_OPEN_PARTITIONS" in str(e):
                 return "TOO_MANY_OPEN_PARTITIONS"
             raise e
@@ -1466,7 +1465,7 @@ class AthenaAdapter(SQLAdapter):
             try:
                 self._run_query(query, catch_partitions_limit=False)
                 break
-            except OperationalError as e:
+            except AthenaError as e:
                 if f"ICEBERG_{op.upper()}_MORE_RUNS_NEEDED" not in str(e):
                     raise e
 
@@ -1477,7 +1476,7 @@ class AthenaAdapter(SQLAdapter):
         LOGGER.debug(f"Running Athena query:\n{query}")
         try:
             cursor.execute(query, catch_partitions_limit=catch_partitions_limit)
-        except OperationalError as e:
+        except AthenaError as e:
             LOGGER.debug(f"CAUGHT EXCEPTION: {e}")
             raise e
         return cursor
