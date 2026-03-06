7c73974efc4f91d42bb76ada28ad50da8418a954
Annebelle Olminkhof <20630578+annebelleo@users.noreply.github.com>
2026-03-06 09:36:45 +0100

    isolate change to dbt-spark

--- dbt-adapters/src/dbt/adapters/contracts/connection.py
+++ dbt-adapters/src/dbt/adapters/contracts/connection.py
@@ -165,10 +165,6 @@ class Credentials(ExtensibleDbtClassMixin, Replaceable, metaclass=abc.ABCMeta):
         data = super().__pre_deserialize__(data)
         # Need to fixup dbname => database, pass => password
         data = cls.translate_aliases(data)
-        # Coerce explicitly to avoid mashumaro 3.15+ converting None to "None"
-        for key in ("schema", "database"):
-            if key in data and data[key] is not None:
-                data[key] = str(data[key])
         return data
 
     @classmethod
