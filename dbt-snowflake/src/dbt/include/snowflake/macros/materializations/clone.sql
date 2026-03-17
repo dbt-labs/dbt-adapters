693e212f1ef880a616ee2384bcd9c1bd87a004f7
James Colvin <james@rayatheapp.com>
2026-03-17 10:08:52 -0400

    Fix dbt clone for Snowflake Iceberg tables
    The clone macro now detects whether the source relation is an Iceberg
    table via load_cached_relation and emits CREATE ICEBERG TABLE ... CLONE
    instead of CREATE TABLE ... CLONE. The transient keyword is correctly
    omitted for Iceberg clones since Iceberg tables cannot be transient.
    
    Resolves #1767

--- dbt-snowflake/src/dbt/include/snowflake/macros/materializations/clone.sql
+++ dbt-snowflake/src/dbt/include/snowflake/macros/materializations/clone.sql
@@ -3,8 +3,14 @@
 {% endmacro %}
 
 {% macro snowflake__create_or_replace_clone(this_relation, defer_relation) %}
+    {%- set source_relation = load_cached_relation(defer_relation) -%}
+    {%- set is_iceberg = source_relation is not none and source_relation.is_iceberg_format -%}
     create or replace
-      {{ "transient" if config.get("transient", true) }}
+      {% if is_iceberg -%}
+        iceberg
+      {%- else -%}
+        {{ "transient" if config.get("transient", true) }}
+      {%- endif %}
       table {{ this_relation }}
       clone {{ defer_relation }}
       {{ "copy grants" if config.get("copy_grants", false) }}
