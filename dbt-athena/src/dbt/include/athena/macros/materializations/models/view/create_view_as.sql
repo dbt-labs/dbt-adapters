f16495588263b402b097029e557893b5e26ae871
Alireza Davoudi <davoudialireza@gmail.com>
2026-04-13 08:39:29 -0700

    feat(athena): add support for Data Catalog Views (Multi Dialect Views)
    Add `is_data_catalog_view` config flag on the view materialization to
    conditionally emit MDV DDL (`CREATE OR REPLACE PROTECTED MULTI DIALECT
    VIEW ... SECURITY DEFINER`) instead of standard `CREATE OR REPLACE VIEW`.
    
    Resolves #1844

--- dbt-athena/src/dbt/include/athena/macros/materializations/models/view/create_view_as.sql
+++ dbt-athena/src/dbt/include/athena/macros/materializations/models/view/create_view_as.sql
@@ -1,10 +1,28 @@
 {% macro athena__create_view_as(relation, sql) -%}
   {%- set contract_config = config.get('contract') -%}
+  {%- set is_data_catalog_view = config.get('is_data_catalog_view', False) -%}
+
+  {%- if is_data_catalog_view and contract_config.enforced -%}
+    {% do exceptions.raise_compiler_error(
+      "Data Catalog Views (is_data_catalog_view=True) do not support contract enforcement (contract.enforced=True). "
+      ~ "Please disable one of these options for model '" ~ model.name ~ "'."
+    ) %}
+  {%- endif -%}
+
   {%- if contract_config.enforced -%}
     {{ get_assert_columns_equivalent(sql) }}
   {%- endif -%}
+
+  {%- if is_data_catalog_view -%}
+  create or replace protected multi dialect view
+    {{ relation }}
+  security definer
+  as
+    {{ sql }}
+  {%- else -%}
   create or replace view
     {{ relation }}
   as
     {{ sql }}
+  {%- endif -%}
 {% endmacro %}
