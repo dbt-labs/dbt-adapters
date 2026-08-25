2a01378b5926f4bf95b33b5c3bdc96c4e9e5cd58
Naman Goyal <43905880+itsnamangoyal@users.noreply.github.com>
2026-08-25 17:57:49 +0530

    fix: gate interactive_table target_lag/warehouse DDL clauses on is_dynamic

--- dbt-snowflake/src/dbt/include/snowflake/macros/relations/interactive_table/create.sql
+++ dbt-snowflake/src/dbt/include/snowflake/macros/relations/interactive_table/create.sql
@@ -15,8 +15,10 @@
 
     create interactive table {{ relation }}
         cluster by ({{ interactive_table.cluster_by }})
-        {% if interactive_table.target_lag is not none %}target_lag = '{{ interactive_table.target_lag }}'{% endif %}
+        {% if interactive_table.is_dynamic %}
+        target_lag = '{{ interactive_table.target_lag }}'
         {{ optional('warehouse', interactive_table.warehouse_parameter, equals_char='= ') }}
+        {% endif %}
         {{ optional('initialization_warehouse', interactive_table.snowflake_initialization_warehouse, equals_char='= ') }}
         as (
             {{ sql }}
